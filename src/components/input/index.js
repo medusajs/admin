bec33808914186966bc0d0abf096f051cd8835bb
olivermrbl <oliver@mrbltech.com>
2020-06-11 13:32:50 +0200

    Emotion ThemeProvider and Rebass impl.

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import { Input } from "@rebass/forms"
+import styled from "@emotion/styled"
+import Typography from "../typography"
 
-const Input = () => <Button>Click Me</Button>
+const StyledInput = styled(Input)`
+  ${Typography.Base}
+`
 
-export default Input
+const InputField = ({ dark, placeholder }) => {
+  return (
+    <StyledInput
+      variant="largeInput"
+      width={350}
+      height={30}
+      placeholder={placeholder ? placeholder : "Placeholder"}
+    />
+  )
+}
+
+export default InputField
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,15 +1,6 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import PropTypes from "prop-types"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,20 +16,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <footer>{new Date().getFullYear()}</footer>
       </div>
     </>
   )
