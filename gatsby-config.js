ebe23330e1e99654acfaa45071e810c2536ee7f6
Sebastian Rindom <skrindom@gmail.com>
2020-06-21 08:59:06 +0200

    Not using filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    //{
+    //  resolve: `gatsby-source-filesystem`,
+    //  options: {
+    //    name: `images`,
+    //    path: `${__dirname}/src/images`,
+    //  },
+    //},
     {
       resolve: "gatsby-plugin-svgr",
       options: {
