e3e8c22f277430cc0d576a988a1a04efbaa5e0ac
olivermrbl <oliver@mrbltech.com>
2020-06-26 09:06:10 +0200

    Fixes sidebar icons

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { Flex, Box, Text, Link } from "rebass"
+import styled from "@emotion/styled"
 import { Container, InlineLogoContainer, LogoContainer } from "./elements"
 import { ReactComponent as Settings } from "../../assets/svg/settings.svg"
+import { ReactComponent as Orders } from "../../assets/svg/orders.svg"
+import { ReactComponent as Products } from "../../assets/svg/products.svg"
+import { ReactComponent as Customers } from "../../assets/svg/customers.svg"
+import { ReactComponent as Discounts } from "../../assets/svg/discounts.svg"
 import { ReactComponent as Logo } from "../../assets/svg/logo.svg"
 import { ReactComponent as LogoInline } from "../../assets/svg/logo-horizontal.svg"
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
 
 const Sidebar = ({}) => {
   return (
@@ -18,37 +28,29 @@ const Sidebar = ({}) => {
       </Flex>
 
       <Flex py={4} mx={-1} flexDirection="column" flex={1}>
-        <Flex py={1}>
-          <Box mx={1} width={1 / 12}>
-            <Settings />
-          </Box>
-          <Box mx={1}>
-            <Link>Orders</Link>
-          </Box>
+        <Flex py={1} alignItems="center">
+          <Orders />
+          <StyledLink width={9 / 10} ml={2} variant="nav" href="/a/orders">
+            Orders
+          </StyledLink>
         </Flex>
-        <Flex py={1}>
-          <Box mx={1} width={1 / 12}>
-            <Settings />
-          </Box>
-          <Box mx={1}>
-            <Link>Products</Link>
-          </Box>
+        <Flex py={1} alignItems="center">
+          <Products />
+          <StyledLink ml={2} variant="nav" href="/a/products">
+            Products
+          </StyledLink>
         </Flex>
-        <Flex py={1}>
-          <Box mx={1} width={1 / 12}>
-            <Settings />
-          </Box>
-          <Box mx={1}>
-            <Link>Customers</Link>
-          </Box>
+        <Flex py={1} alignItems="center">
+          <Customers />
+          <StyledLink ml={2} variant="nav" href="/a/customers">
+            Customers
+          </StyledLink>
         </Flex>
-        <Flex py={1}>
-          <Box mx={1} width={1 / 12}>
-            <Settings />
-          </Box>
-          <Box mx={1}>
-            <Link>Shipping</Link>
-          </Box>
+        <Flex py={1} alignItems="center">
+          <Discounts />
+          <StyledLink ml={2} variant="nav" href="/a/discounts">
+            Discounts
+          </StyledLink>
         </Flex>
       </Flex>
       <Flex mx={-1} alignItems="center" justifyContent="space-between ">
