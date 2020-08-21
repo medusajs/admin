e4945365bdec780793dcd31461dd58dd0ca635c5
Sebastian Rindom <skrindom@gmail.com>
2020-08-21 14:57:53 +0200

    Skip react json view

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,14 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-json-view/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
