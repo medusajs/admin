9e02e36d02f499e6431ca72cb6c040cd56510874
pKorsholm <philip.korsholm@hotmail.com>
2021-09-14 10:00:15 +0200

    camel casing of local variables

--- src/domain/orders/details/utils/create-filtering.js
+++ src/domain/orders/details/utils/create-filtering.js
@@ -1,14 +1,17 @@
-export const filterItems = (order, is_claim) => {
-  let tmpp = order.items.reduce(
-    (map, obj) => map.set(obj.id, { ...obj }),
+export const filterItems = (order, isClaim) => {
+  let orderItems = order.items.reduce(
+    (map, obj) =>
+      map.set(obj.id, {
+        ...obj,
+      }),
     new Map()
   )
 
-  let claim_items = []
+  let claimItems = []
 
   if (order.claims && order.claims.length) {
     for (const s of order.claims) {
-      claim_items = [...claim_items, ...s.claim_items]
+      claimItems = [...claimItems, ...s.claim_items]
 
       //
       //    Ticket is created to allow claims and swaps on claims and swaps without errors
@@ -21,34 +24,37 @@ export const filterItems = (order, is_claim) => {
       //   }
 
       //   if (s.additional_items && s.additional_items.length)
-      //     tmpp = s.additional_items
+      //     orderItems = s.additional_items
       //       .filter(
       //         it =>
       //           it.shipped_quantity ||
       //           it.shipped_quantity === it.fulfilled_quantity
       //       )
-      //       .reduce((map, obj) => map.set(obj.id, { ...obj }), tmpp)
+      //       .reduce((map, obj) => map.set(obj.id, { ...obj }), orderItems)
     }
   }
 
-  if (!is_claim) {
+  if (!isClaim) {
     if (order.swaps && order.swaps.length) {
       for (const s of order.swaps) {
-        tmpp = s.additional_items.reduce(
-          (map, obj) => map.set(obj.id, { ...obj }),
-          tmpp
+        orderItems = s.additional_items.reduce(
+          (map, obj) =>
+            map.set(obj.id, {
+              ...obj,
+            }),
+          orderItems
         )
       }
     }
   }
 
-  for (const item of claim_items) {
-    let i = tmpp.get(item.item_id)
+  for (const item of claimItems) {
+    let i = orderItems.get(item.item_id)
     if (i) {
       i.quantity = i.quantity - item.quantity
-      i.quantity !== 0 ? tmpp.set(i.id, i) : tmpp.delete(i.id)
+      i.quantity !== 0 ? orderItems.set(i.id, i) : orderItems.delete(i.id)
     }
   }
 
-  return [...tmpp.values()]
+  return [...orderItems.values()]
 }
