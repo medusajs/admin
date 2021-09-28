2b09cabfbcffbba5354c7f6fcdb7b34a77e150b0
Philip Korsholm <philip.korsholm@hotmail.com>
2021-09-28 13:18:54 +0200

    breadcrumb component

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -5,8 +5,13 @@ import { Link, navigate } from "gatsby"
 
 const BreadCrumb = ({ previousRoute, previousBreadCrumb, currentPage }) => {
   return (
-    <Flex width={1} sx={{ fontSize: "14px" }}>
-      <Text mr={1} color="gray">
+    <Flex width={1} pb={3} sx={{ fontSize: "14px", fontWeight: 400 }}>
+      <Text
+        mr={1}
+        onClick={() => navigate(previousRoute)}
+        sx={{ cursor: "pointer" }}
+        color="gray"
+      >
         {previousBreadCrumb}
       </Text>
       <Text mr={1} color="gray">
