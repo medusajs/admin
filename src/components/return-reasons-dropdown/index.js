17081bbd3827f80732d684a6849171e67c7f9428
Philip Korsholm <philip.korsholm@hotmail.com>
2021-09-30 08:34:51 +0200

    refactor styles

--- src/components/return-reasons-dropdown/index.js
+++ src/components/return-reasons-dropdown/index.js
@@ -1,13 +1,61 @@
 import React, { useEffect, useState, useRef, Children } from "react"
-import { Box, Flex, Text } from "rebass"
-import { Label } from "@rebass/forms"
-import styled from "@emotion/styled"
-import Typography from "../typography"
+import { Flex } from "rebass"
 import useMedusa from "../../hooks/use-medusa"
 import { ReactSelect } from "../react-select"
 import { components } from "react-select"
 import { ReactComponent as ArrowDown } from "../../assets/svg/arrow-down.svg"
 import { ReactComponent as ArrowUp } from "../../assets/svg/arrow-up.svg"
+
+const groupHeadingStyle = {
+  heigth: "30px",
+  pr: "3",
+  py: "0",
+  borderBottom: "1px solid #E3E8EE",
+  my: "0",
+  "& div": {
+    pb: "0",
+    pt: "0",
+    width: "100%",
+    lineHeight: "30px",
+    color: "#000000CC",
+    fontSize: "11px",
+    height: "30px",
+  },
+  "& svg": {
+    mb: "1",
+  },
+}
+
+const menuStyle = {
+  minWidth: "140px",
+  width: "auto",
+  "& div": {
+    minWidth: "140px",
+    width: "auto",
+    maxWidth: "250px",
+    overflow: "hidden",
+  },
+}
+
+const groupStyle = {
+  width: "100%",
+  py: "0",
+  "& div": { py: "0", width: "100%" },
+}
+
+const optionStyle = {
+  height: "30px",
+  my: "0",
+  width: "100%",
+  py: "0",
+  "& div": {
+    my: "0",
+    py: "0",
+    width: "100%",
+    lineHeight: "30px",
+    height: "30px",
+  },
+}
 
 const ReturnReasonsDropdown = ({ setReturnReason }) => {
   const { return_reasons, isLoading } = useMedusa("returnReasons")
@@ -31,14 +79,7 @@ const ReturnReasonsDropdown = ({ setReturnReason }) => {
     )
   }, [isLoading])
 
-  if (isLoading) {
-    return <></>
-  }
-
-  // handle options group header click event
-  // hide and show the options under clicked group
   const handleHeaderClick = props => {
-    console.log(props)
     setGroupedOptions(
       groupedOptions.map(groupedOption =>
         groupedOption.label === props.data.label
@@ -48,34 +89,13 @@ const ReturnReasonsDropdown = ({ setReturnReason }) => {
     )
   }
 
-  // Create custom GroupHeading component, which will wrap
-  // react-select GroupHeading component inside a div and
-  // register onClick event on that div
   const CustomGroupHeading = props => {
     return (
       <Flex
         alignItems="center"
         justifyContent="space-between"
         width={1}
-        sx={{
-          heigth: "30px",
-          pr: "3",
-          py: "0",
-          borderBottom: "1px solid #E3E8EE",
-          my: "0",
-          "& div": {
-            pb: "0",
-            pt: "0",
-            width: "100%",
-            lineHeight: "30px",
-            color: "#000000CC",
-            fontSize: "11px",
-            height: "30px",
-          },
-          "& svg": {
-            mb: "1",
-          },
-        }}
+        sx={groupHeadingStyle}
         onClick={() => handleHeaderClick(props)}
       >
         <components.GroupHeading {...props} />
@@ -90,18 +110,7 @@ const ReturnReasonsDropdown = ({ setReturnReason }) => {
 
   const customMenu = props => {
     return (
-      <Flex
-        sx={{
-          minWidth: "140px",
-          width: "auto",
-          "& div": {
-            minWidth: "140px",
-            width: "auto",
-            maxWidth: "250px",
-            overflow: "hidden",
-          },
-        }}
-      >
+      <Flex sx={menuStyle}>
         <components.Menu {...props} />
       </Flex>
     )
@@ -113,35 +122,16 @@ const ReturnReasonsDropdown = ({ setReturnReason }) => {
       rest.children = children
     }
     return (
-      <Flex
-        sx={{
-          width: "100%",
-          py: "0",
-          "& div": { py: "0", width: "100%" },
-        }}
-      >
+      <Flex sx={groupStyle}>
         <components.Group {...rest} />
       </Flex>
     )
   }
 
   const CustomOption = props => {
-    const st = {
-      height: "30px",
-      my: "0",
-      width: "100%",
-      py: "0",
-      "& div": {
-        my: "0",
-        py: "0",
-        width: "100%",
-        lineHeight: "30px",
-        height: "30px",
-      },
-    }
     const style = props.data.parent_return_reason_id
-      ? { bg: "light", borderBottom: "1px solid #E3E8EE", ...st }
-      : st
+      ? { bg: "light", borderBottom: "1px solid #E3E8EE", ...optionStyle }
+      : optionStyle
 
     return (
       <Flex sx={style}>
