0c9126203768a2c0127caaa7774b03204e3aa0b7
Philip Korsholm <philip.korsholm@hotmail.com>
2021-10-06 10:35:47 +0200

    forgot password functionality and ui update

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react"
-import { navigate } from "gatsby"
+import { navigate, Link } from "gatsby"
 import { Router } from "@reach/router"
 import { Flex, Box, Text, Image } from "rebass"
 import { useForm } from "react-hook-form"
@@ -11,24 +11,31 @@ import InputField from "../components/input"
 import Button from "../components/button"
 import Spinner from "../components/spinner"
 import Graphic from "../assets/login-graphic.png"
-
-import MedusaClient from "../services/api"
+import Medusa from "../services/api"
 
 const IndexPage = () => {
   const [loading, setLoading] = useState(false)
   const account = useContext(AccountContext)
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, reset } = useForm()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [resetPassword, setResetPassword] = useState(false)
 
   const handleLogin = data => {
     setLoading(true)
-    account
-      .handleLogin(data)
-      .then(() => {
+    if (resetPassword) {
+      Medusa.users.resetPasswordToken({ email }).then(data => {
+        console.log(data)
+        setEmail("")
+        setPassword("")
+        setResetPassword(false)
+      })
+    } else {
+      account.handleLogin({ email, password }).then(() => {
         navigate("/a")
       })
-      .finally(() => {
-        setLoading(false)
-      })
+    }
+    setLoading(false)
   }
 
   return (
@@ -58,41 +65,89 @@ const IndexPage = () => {
           width={5 / 12}
           sx={{ zIndex: 9999 }}
           flexDirection="column"
+          alignItems="center"
           mx="auto"
           as="form"
           height="400px"
           onSubmit={handleSubmit(handleLogin)}
         >
-          <Box variant={"loginCard"} p={5} height="100%">
-            <Text mb={4} fontWeight="bold" fontSize={4}>
-              Sign in to your account
-            </Text>
-            {loading ? (
-              <Flex justifyContent="center">
-                <Spinner dark width="20px" height="20px" />
-              </Flex>
+          <Box width={1} variant={"loginCard"} p={4} justifyContent="center">
+            {resetPassword ? (
+              <>
+                <Text mb={2} fontWeight="bold" fontSize={4}>
+                  Reset Password
+                </Text>
+                <Flex width={1} flexDirection="column" alignItems="center">
+                  <Flex width={1} justifyContent="flex-start">
+                    <Text
+                      onClick={() => setResetPassword(false)}
+                      fontSize={2}
+                      mb={2}
+                      color="blue"
+                    >
+                      Back
+                    </Text>
+                  </Flex>
+                  <InputField
+                    my={3}
+                    width="100%"
+                    label="Email"
+                    name="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    boldLabel={true}
+                  />
+                  <Button type="submit" variant={"cta"} mt={3} width={1 / 4}>
+                    Submit
+                  </Button>
+                </Flex>
+              </>
             ) : (
               <>
-                <InputField
-                  mb={3}
-                  label="Email"
-                  name="email"
-                  ref={register}
-                  boldLabel={true}
-                />
-                <InputField
-                  type="password"
-                  label="Password"
-                  boldLabel={true}
-                  name="password"
-                  ref={register}
-                />
-                <Button type="submit" variant={"cta"} mt={4} width={1}>
-                  Continue
-                </Button>
+                <Text mb={4} fontWeight="bold" fontSize={4}>
+                  Sign in
+                </Text>
+                {loading ? (
+                  <Flex justifyContent="center">
+                    <Spinner dark width="20px" height="20px" />
+                  </Flex>
+                ) : (
+                  <Flex width={1} flexDirection="column" alignItems="center">
+                    <InputField
+                      mb={3}
+                      width="100%"
+                      label="Email"
+                      name="email"
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
+                      boldLabel={true}
+                    />
+                    <InputField
+                      mb={3}
+                      type="password"
+                      width="100%"
+                      label="Password"
+                      boldLabel={true}
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                    />
+                    <Button type="submit" variant={"cta"} mt={4} width={1 / 4}>
+                      Login
+                    </Button>
+                  </Flex>
+                )}
               </>
             )}
           </Box>
+          {!resetPassword && (
+            <Text
+              onClick={() => setResetPassword(true)}
+              pt={1}
+              sx={{ fontSize: 1, color: "gray" }}
+            >
+              Forgot your password? Click here
+            </Text>
+          )}
         </Flex>
       </Flex>
     </LoginLayout>
