60529c54890d199b6e6b8235e7035e9219661408
zakariaelas <zakaria.elas@gmail.com>
2021-10-18 12:36:23 +0100

    upgrade storybook

--- src/components/dropdown/dropdown.stories.js
+++ src/components/dropdown/dropdown.stories.js
@@ -6,39 +6,72 @@ import Dropdown from "./index"
 
 const StyledContainer = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
 `
 
-const Container = () => {
+const Container = args => {
   const [values, setValues] = useState([])
 
   const handleChange = values => {
     setValues(values)
   }
 
+  console.log({ args })
+
   return (
     <StyledContainer>
-      <div>
-        <Dropdown
-          searchPlaceholder={"SKU, Product Name, Size, etc."}
-          showSearch
-        >
-          <div>Prod 1</div>
-          <div>Prod 2</div>
-          <div>Prod 3</div>
-          <div>Prod 4</div>
-          <div>Prod 5</div>
-          <div>Prod 6</div>
-          <div>Prod 7</div>
-        </Dropdown>
-      </div>
+      <Dropdown {...args}>
+        <div>Prod 1</div>
+        <div>Prod 2</div>
+        <div>Prod 3</div>
+        <div>Prod 4</div>
+        <div>Prod 5</div>
+        <div>Prod 6</div>
+        <div>Prod 7</div>
+      </Dropdown>
     </StyledContainer>
   )
 }
 
-export default {
-  title: `Dropdown`,
+const Template = args => <Container {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  searchPlaceholder: "SKU, Product Name, Size, etc.",
+  showSearch: true,
+  showTrigger: true,
+  topPlacement: 10,
+  placement: "bottom-start",
+  dropdownWidth: 160,
+  dropdownHeight: 300,
+  toggleText: "",
 }
 
-export const default_ = () => <Container />
+export default {
+  title: `Dropdown`,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    placement: {
+      options: [
+        "top-start",
+        "top",
+        "top-end",
+        "right-start",
+        "right",
+        "right-end",
+        "bottom-start",
+        "bottom",
+        "bottom-end",
+        "left-start",
+        "left",
+        "left-end",
+      ],
+      control: { type: "select" },
+    },
+  },
+}
