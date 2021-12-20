e4abc18614d3f5e156ef518f40ca0fdfe5ad7574
Kasper <kasper@medusa-commerce.com>
2021-12-20 16:44:28 +0100

    fix: commented out other css files as they were interfering with Tailwind styling

--- src/components/atoms/page-description.js
+++ src/components/atoms/page-description.js
@@ -3,7 +3,7 @@ import React from "react"
 const PageDescription = ({ title = null, subtitle = null }) => {
   return (
     <div className="mb-xlarge">
-      <h1 className="inter-2xlarge-smibold mb-xsmall">{title}</h1>
+      <h1 className="inter-2xlarge-semibold mb-xsmall">{title}</h1>
       <h2 className="inter-base-regular">{subtitle}</h2>
     </div>
   )
