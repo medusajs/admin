1d2bc6417b6633d0034c8822ad5a234df27febc3
Philip Korsholm <philip.korsholm@hotmail.com>
2021-12-22 09:27:17 +0100

    refactor colors

--- src/components/molecules/sidebar-team-member/index.tsx
+++ src/components/molecules/sidebar-team-member/index.tsx
@@ -1,28 +1,20 @@
 import React from 'react'
 
-const getInitial = (firstName, lastName) => {
-  // return `${firstName?.charAt(0)?.toUpperCase() || ""}${lastName?.charAt(0)?.toUpperCase() || ""}`
-  return firstName?.charAt(0)?.toUpperCase() || lastName?.charAt(0)?.toUpperCase() || ""
+const getInitial = (firstName, lastName, email) => {
+  return firstName?.charAt(0)?.toUpperCase() || lastName?.charAt(0)?.toUpperCase() || email?.charAt(0)?.toUpperCase() || ""
 }
 
-const getColor = (index) => {
-  const colors = ["bg-fuschia-40", "bg-pink-40", "bg-orange-40", "bg-teal-40", "bg-cyan-40", "bg-blue-40", "bg-indigo-40"]
-  return colors[index % colors.length]
-}
-
-const SidebarTeamMember = ({ index, user, profilePicture = undefined }) => {
+const SidebarTeamMember = ({ color = 'bg-violet-40', user, profilePicture = undefined }) => {
 
   const fullName = user.first_name || user.last_name ? `${user.first_name} ${user.last_name}` : user.email
 
-  const avatarClasses = `w-5 h-5 ${getColor(index)} text-grey-0 rounded-full text-center flex justify-center items-center`
-
-  console.log(getColor(index))
+  const avatarClasses = `w-5 h-5 ${color} text-grey-0 rounded-full text-center flex justify-center items-center`
 
   const avatar = profilePicture ? (
     <img className="w-5 h-5 mb-0 object-cover rounded-full" src={profilePicture} />
   ) :
     (
-      <div className={avatarClasses} > {getInitial(user.first_name, user.last_name)}</div>
+      <div className={avatarClasses} > {getInitial(user.first_name, user.last_name, user.email)}</div>
     )
 
   return (
