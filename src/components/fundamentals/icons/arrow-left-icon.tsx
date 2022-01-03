1d52309002787977cb0ada7e58b5b53d506d6a4a
Philip Korsholm <philip.korsholm@hotmail.com>
2022-01-03 13:51:54 +0100

    viewport update

--- src/components/fundamentals/icons/arrow-left-icon.tsx
+++ src/components/fundamentals/icons/arrow-left-icon.tsx
@@ -9,7 +9,7 @@ const ArrowLeftIcon: React.FC<IconProps> = ({
     <svg
       width={size}
       height={size}
-      viewBox={`0 0 ${size} ${size}`}
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
--- src/components/fundamentals/icons/gear-icon.tsx
+++ src/components/fundamentals/icons/gear-icon.tsx
@@ -9,7 +9,7 @@ const GearIcon: React.FC<IconProps> = ({
     <svg
       width={size}
       height={size}
-      viewBox={`0 0 ${size} ${size}`}
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
