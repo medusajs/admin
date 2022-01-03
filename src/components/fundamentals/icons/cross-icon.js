b36a98938a3990c51644ec5f574ac5cff490b89a
Philip Korsholm <philip.korsholm@hotmail.com>
2022-01-03 11:53:43 +0100

    icon update for modal and formatting

--- src/components/fundamentals/icons/cross-icon.js
+++ src/components/fundamentals/icons/cross-icon.js
@@ -1,28 +1,26 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-
-
 const CrossIcon = props => {
   const { size, color, ...rest } = props
   return (
     <svg
       width={size}
       height={size}
-      viewBox="0 0 20 20"
+      viewBox={`0 0 ${size} ${size}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M15 5L5 15"
-        stroke="inherit"
+        stroke={color}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M5 5L15 15"
-        stroke="inherit"
+        stroke={color}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
--- src/components/molecules/modal/index.js
+++ src/components/molecules/modal/index.js
@@ -1,57 +1,71 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react"
+import PropTypes from "prop-types"
 import { ReactComponent as CloseIcon } from "../../../assets/svg/2.0/20px/x.svg"
-import * as Dialog from '@radix-ui/react-dialog'
+import CrossIcon from "../../fundamentals/icons/cross-icon"
+import * as Dialog from "@radix-ui/react-dialog"
 
-const Overlay = ({children}) => {
-  return (<Dialog.Overlay className='fixed bg-grey-90/40 grid top-0 left-0 right-0 bottom-0 place-items-center overflow-y-auto'>
-    {children}
-  </Dialog.Overlay>)
+const Overlay = ({ children }) => {
+  return (
+    <Dialog.Overlay className="fixed bg-grey-90/40 grid top-0 left-0 right-0 bottom-0 place-items-center overflow-y-auto">
+      {children}
+    </Dialog.Overlay>
+  )
 }
 
-const Content = ({children}) => {
-  return (<Dialog.Content className='bg-grey-0 min-w-modal rounded'>
-    {children}
-  </Dialog.Content>)
+const Content = ({ children }) => {
+  return (
+    <Dialog.Content className="bg-grey-0 min-w-modal rounded">
+      {children}
+    </Dialog.Content>
+  )
 }
 
 const addProp = (children, prop) => {
   return React.Children.map(children, c => React.cloneElement(c, prop))
 }
 
-const Modal = ({children, handleClose, isLargeModal = true}) => {
+const Modal = ({ children, handleClose, isLargeModal = true }) => {
   const largeModal = isLargeModal
   return (
     <Dialog.Root open={true} onOpenChange={handleClose}>
       <Dialog.Portal>
         <Overlay>
-          <Content>
-            {addProp(children, largeModal)}
-          </Content> 
+          <Content>{addProp(children, largeModal)}</Content>
         </Overlay>
       </Dialog.Portal>
-    </Dialog.Root>)
+    </Dialog.Root>
+  )
 }
 
 Modal.Body = ({ children, largeModal = true }) => {
   return (
-    <div className='inter-base-regular' onClick={e => e.stopPropagation()}>
+    <div className="inter-base-regular" onClick={e => e.stopPropagation()}>
       {addProp(children, largeModal)}
     </div>
   )
 }
 
 Modal.Content = ({ children, largeModal = true }) => {
- return (<div className={`px-7 pt-5 ${largeModal ? "w-[750px] pb-7" : "pb-5"}`}> {children}</div>)
+  return (
+    <div className={`px-7 pt-5 ${largeModal ? "w-[750px] pb-7" : "pb-5"}`}>
+      {" "}
+      {children}
+    </div>
+  )
 }
 
 Modal.Header = ({ children, handleClose = undefined }) => {
   return (
-    <div className='pl-7 pt-3.5 pr-3.5 flex flex-col w-full' onClick={e => e.stopPropagation()}>
-      <div className='pb-1 flex w-full justify-end'>
-        {handleClose && (<span onClick={handleClose} className="stroke-grey-50 cursor-pointer">
-          <CloseIcon />
-        </span>)}
+    <div
+      className="pl-7 pt-3.5 pr-3.5 flex flex-col w-full"
+      onClick={e => e.stopPropagation()}
+    >
+      <div className="pb-1 flex w-full justify-end">
+        {handleClose && (
+          <span onClick={handleClose} className="text-grey-50 cursor-pointer">
+            <CrossIcon size={20} />
+          </span>
+        )}
       </div>
       {children}
     </div>
@@ -60,19 +74,24 @@ Modal.Header = ({ children, handleClose = undefined }) => {
 
 Modal.Footer = ({ children, largeModal = true }) => {
   return (
-    <div onClick={e => e.stopPropagation()} className={`px-7  pb-5 flex w-full ${largeModal ? "border-t border-grey-20 pt-3.5": ""}`}>
+    <div
+      onClick={e => e.stopPropagation()}
+      className={`px-7  pb-5 flex w-full ${
+        largeModal ? "border-t border-grey-20 pt-3.5" : ""
+      }`}
+    >
       {children}
     </div>
   )
 }
 
-Modal.Header.propTypes = { 
-  handleClose: PropTypes.func, 
+Modal.Header.propTypes = {
+  handleClose: PropTypes.func,
 }
 
 Modal.propTypes = {
   isLargeModal: PropTypes.bool,
-  handleClose: PropTypes.func, 
+  handleClose: PropTypes.func,
 }
 
 export default Modal
