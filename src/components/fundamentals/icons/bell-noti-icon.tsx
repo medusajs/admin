6e515458a6f64cb10ba04f9a4e53bf40e088c520
Philip Korsholm <philip.korsholm@hotmail.com>
2022-01-03 13:37:35 +0100

    fix viewport

--- src/components/fundamentals/icons/bell-noti-icon.tsx
+++ src/components/fundamentals/icons/bell-noti-icon.tsx
@@ -13,7 +13,7 @@ const BellNotiIcon: React.FC<IBellNotiIconProps> = ({
     <svg
       width={size}
       height={size}
-      viewBox={`0 0 ${size} ${size}`}
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
--- src/components/fundamentals/icons/search-icon.tsx
+++ src/components/fundamentals/icons/search-icon.tsx
@@ -9,7 +9,7 @@ const SearchIcon: React.FC<IIconProps> = ({
     <svg
       width={size}
       height={size}
-      viewBox={`0 0 ${size} ${size}`}
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
