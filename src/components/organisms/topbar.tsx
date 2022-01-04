79b7872e8c6eac73e62691d78b5079a685c5790b
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2022-01-04 13:49:28 +0100

    fix: revamp ts convert icons (#217)

--- src/components/organisms/topbar.tsx
+++ src/components/organisms/topbar.tsx
@@ -3,12 +3,12 @@ import Avatar from "../atoms/avatar"
 import NotificationBell from "../molecules/notification-bell"
 import SearchBar from "../molecules/search-bar"
 
-const Topbar = () => {
+const Topbar: React.FC = () => {
   return (
     <div className="w-full min-h-topbar max-h-topbar pr-xlarge pl-base bg-grey-0 border-b border-grey-20 sticky top-0 flex items-center justify-between z-50">
       <SearchBar />
       <div className="flex items-center">
-        <NotificationBell />
+        <NotificationBell hasNotifications={false} />
         <div className="ml-large">
           <Avatar />
         </div>
