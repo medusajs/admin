09b8721936d7b45005a88a3bd9319b1c6f351641
Philip Korsholm <philip.korsholm@hotmail.com>
2022-01-05 08:24:37 +0100

    make dropdown input container

--- src/components/multi-select/index.js
+++ src/components/multi-select/index.js
@@ -1,15 +1,16 @@
-import React from "react"
-import { Text, Flex } from "rebass"
+import React, { useImperativeHandle, useRef, useState } from "react"
+import { Flex } from "rebass"
 import { Label } from "@rebass/forms"
 import { default as ReactMultiSelect } from "react-multi-select-component"
 import styled from "@emotion/styled"
-import Typography from "../typography"
+import InputContainer from "../fundamentals/input-container"
+import InputHeader from "../fundamentals/input-header"
 
 const StyledMultiSelect = styled(ReactMultiSelect)`
-  ${Typography.Base}
-
   color: black;
-  background-color: white;
+  background-color: inherit;
+
+  padding: 0;
 
   height: 33px;
 
@@ -29,48 +30,11 @@ const StyledMultiSelect = styled(ReactMultiSelect)`
   transition: all 0.2s ease;
 
   border-radius: 3px;
-  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
-    rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(60, 66, 87, 0.16) 0px 0px 0px 1px,
-    rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
-    rgba(0, 0, 0, 0) 0px 0px 0px 0px;
-
-  &:focus: {
-    box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px,
-      rgba(206, 208, 190, 0.36) 0px 0px 0px 4px,
-      rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(60, 66, 87, 0.16) 0px 0px 0px 1px,
-      rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
-      rgba(0, 0, 0, 0) 0px 0px 0px 0px;
-  }
-  &::placeholder: {
-    color: #a3acb9;
-  }
 
   .go3433208811 {
     border: none;
     border-radius: 3px;
   }
-`
-
-const StyledLabel = styled.div`
-  ${Typography.Base}
-  ${props =>
-    props.inline
-      ? `
-  text-align: right;
-  padding-right: 15px;
-  `
-      : `
-  padding-bottom: 10px;
-  `}
-  
-  ${props =>
-    props.required &&
-    `
-  &:after {
-    color: rgba(255, 0, 0, 0.5);
-    content: " *";
-  }
-  `}
 `
 
 const MultiSelect = React.forwardRef(
@@ -90,26 +54,17 @@ const MultiSelect = React.forwardRef(
     },
     ref
   ) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const dropdownRef = useRef(null)
+
+    useImperativeHandle(ref, () => dropdownRef.current)
+
     return (
-      <Flex
-        alignItems={inline && "center"}
-        flexDirection={inline ? "row" : "column"}
-        width={props.width}
-        {...props}
-      >
-        {label && (
-          <Label
-            flex={"30% 0 0"}
-            htmlFor={name}
-            display={props.start ? "flex" : inline && "inline !important"}
-          >
-            <StyledLabel required={required} inline={inline}>
-              {label}
-            </StyledLabel>
-          </Label>
-        )}
+      <InputContainer key={name} onClick={() => setIsOpen(!isOpen)}>
+        <InputHeader {...{ label, required }} />
         <StyledMultiSelect
-          ref={ref}
+          ref={dropdownRef}
+          isOpen={isOpen}
           inline={inline}
           name={name}
           type={type}
@@ -119,7 +74,7 @@ const MultiSelect = React.forwardRef(
           overrideStrings={overrideStrings}
           {...selectOptions}
         />
-      </Flex>
+      </InputContainer>
     )
   }
 )
