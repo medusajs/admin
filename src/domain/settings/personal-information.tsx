905cc03d641849b7bbbc7fa6b0f41a875c9a94cd
Philip Korsholm <philip.korsholm@hotmail.com>
2022-01-10 11:19:14 +0100

    initial personal information

--- src/domain/settings/personal-information.tsx
+++ src/domain/settings/personal-information.tsx
@@ -1,27 +1,43 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { useForm } from "react-hook-form"
 import TwoSplitPane from "../../components/templates/two-split-pane"
 import BodyCard from "../../components/organisms/body-card"
-
+import BreadCrumb from "../../components/molecules/breadcrumb"
+import { AccountContext } from "../../context/account"
 import useMedusa from "../../hooks/use-medusa"
-
+import { navigate } from "gatsby"
 import { getErrorMessage } from "../../utils/error-messages"
 import Input from "../../components/molecules/input"
 
 const PersonalInformation = () => {
   const [selectedCurrencies, setCurrencies] = useState([])
   const { register, setValue, handleSubmit } = useForm()
-  const { store, isLoading, update, toaster } = useMedusa("store")
+  const { handleUpdateUser, ...user } = useContext(AccountContext)
+  const { update, toaster, isLoading, ...test } = useMedusa("users", {
+    id: user.id,
+  })
+
+  const [firstName, setFirstName] = useState(user.firstName)
+  const [lastName, setLastName] = useState(user.lastName)
+  const [email, setEmail] = useState(user.email)
+  console.log(user)
+  console.log(test)
+  console.log(update)
 
   useEffect(() => {}, [isLoading])
 
-  const onSubmit = data => {
+  const onSubmit = () => {
     try {
       update({
-        default_currency_code: data.default_currency_code,
-        currencies: selectedCurrencies.map(c => c.value),
-      })
-      toaster("Successfully updated currencies", "success")
+        first_name: firstName,
+        last_name: lastName,
+      }).then(() =>
+        handleUpdateUser({
+          firstName,
+          lastName,
+        })
+      )
+      toaster("Successfully updated user", "success")
     } catch (error) {
       toaster(getErrorMessage(error), "error")
     }
@@ -30,40 +46,62 @@ const PersonalInformation = () => {
   const events = [
     {
       label: "Save",
-      onClick: () => console.log("Save clicked"),
+      onClick: onSubmit,
     },
     {
       label: "Cancel changes",
-      onClick: () => console.log("Cancel clicked"),
+      onClick: () => navigate("/a/settings"),
     },
   ]
 
   return (
-    <TwoSplitPane>
-      <BodyCard
-        title="Personal information"
-        subtitle="Manage your Medusa profile"
-        events={events}
-      >
-        <div>
-          <span className="inter-base-semibold">Picture</span>
-          <div className="w-28 h-28 p-2 mt-2 rounded-rounded hover:bg-grey-5 cursor-pointer">
-            <div className="bg-teal-40 rounded-circle w-full h-full uppercase flex items-center justify-center inter-3xlarge-semibold text-grey-0">
-              PK
+    <div>
+      <BreadCrumb
+        currentPage={"Personal information"}
+        previousBreadcrumb={"Settings"}
+        previousRoute="/a/settings"
+      />
+      <TwoSplitPane>
+        <BodyCard
+          title="Personal information"
+          subtitle="Manage your Medusa profile"
+          events={events}
+        >
+          <div>
+            <span className="inter-base-semibold">Picture</span>
+            <div className="w-28 h-28 p-2 mt-2 rounded-rounded hover:bg-grey-5 cursor-pointer">
+              <div className="bg-teal-40 rounded-circle w-full h-full uppercase flex items-center justify-center inter-3xlarge-semibold text-grey-0">
+                PK
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="mt-6">
-          <span className="inter-base-semibold">General</span>
-          <div className="flex mt-4">
-            <Input label="First name" className="mr-4" />
-            <Input label="Last name" />
+          <div className="mt-6">
+            <span className="inter-base-semibold">General</span>
+            <div className="flex mt-4">
+              <Input
+                label="First name"
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+                className="mr-4"
+              />
+              <Input
+                label="Last name"
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+              />
+            </div>
+            <Input
+              label="Email"
+              value={email}
+              disabled
+              onChange={e => setEmail(e.target.value)}
+              className="mt-6"
+            />
           </div>
-          <Input label="Email" className="mt-6" />
-        </div>
-      </BodyCard>
-    </TwoSplitPane>
+        </BodyCard>
+      </TwoSplitPane>
+    </div>
   )
 }
 
