5e4c1710020422594c6f716a358838827bc2ed69
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-01-10 19:50:59 +0100

    Feat: Revamp avatar (#226)

--- src/components/atoms/avatar.tsx
+++ src/components/atoms/avatar.tsx
@@ -1,14 +1,23 @@
 import React from "react"
+import * as RadixAvatar from "@radix-ui/react-avatar"
+import clsx from "clsx"
 
 type AvatarProps = {
   user?: {
+    img?: string
     first_name?: string
     last_name?: string
     email: string
   }
+  font?: string
+  color?: string
 }
 
-const Avatar: React.FC<AvatarProps> = ({ user }) => {
+const Avatar: React.FC<AvatarProps> = ({
+  user,
+  font = "inter-small-semibold",
+  color = "bg-violet-60",
+}) => {
   let username: string
 
   if (user?.first_name && user?.last_name) {
@@ -20,11 +29,26 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
   }
 
   return (
-    <div className="w-large h-large rounded-circle bg-violet-60 overflow-hidden">
-      <div className="w-full h-full text-center text-grey-0">
+    <RadixAvatar.Root
+      className={clsx(
+        "w-full h-full items-center justify-center overflow-hidden select-none rounded-circle",
+        color
+      )}
+    >
+      <RadixAvatar.Image
+        src={user?.img}
+        alt={username}
+        className="w-full h-full object-cover rounded-circle"
+      />
+      <RadixAvatar.Fallback
+        className={clsx(
+          "w-full h-full flex items-center justify-center bg-inherit text-grey-0 rounded-circle",
+          font
+        )}
+      >
         {username.slice(0, 1).toUpperCase()}
-      </div>
-    </div>
+      </RadixAvatar.Fallback>
+    </RadixAvatar.Root>
   )
 }
 
