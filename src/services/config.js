5afaf8c0428f8751c21e6a5aafec6daa3c754524
zakariaelas <zakaria.elas@gmail.com>
2022-01-12 13:21:17 +0100

    fix: env variable

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,18 @@
 import { QueryClient } from "react-query"
 
-export let medusaUrl =
-  process.env.GATSBY_MEDUSA_BACKEND_URL || "http://localhost:9000"
+let medusaUrl = "http://localhost:9000"
 
-export const queryClient = new QueryClient({
+// deprecated
+if (process.env.GATSBY_STORE_URL) {
+  medusaUrl = process.env.GATSBY_STORE_URL
+}
+
+// takes precedence over GATSBY_STORE_URL
+if (process.env.GATSBY_MEDUSA_BACKEND_URL) {
+  medusaUrl = process.env.GATSBY_MEDUSA_BACKEND_URL
+}
+
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
@@ -12,3 +21,5 @@ export const queryClient = new QueryClient({
     },
   },
 })
+
+export { medusaUrl, queryClient }
