50693669a521883b5a6a296665ea28021906baf2
Philip Korsholm <philip.korsholm@hotmail.com>
2022-01-12 15:09:11 +0100

    remove unused code from user index domain

--- src/domain/settings/users/index.tsx
+++ src/domain/settings/users/index.tsx
@@ -1,47 +1,16 @@
 import React, { useEffect, useState } from "react"
 import BreadCrumb from "../../../components/molecules/breadcrumb"
-import RefreshIcon from "../../../components/fundamentals/icons/refresh-icon"
-import TrashIcon from "../../../components/fundamentals/icons/trash-icon"
-import Table from "../../../components/molecules/table"
-import StatusDot from "../../../components/fundamentals/status-dot"
 import Medusa from "../../../services/api"
-import EditUser from "./edit"
-import DeletePrompt from "../../../components/organisms/delete-prompt"
 import BodyCard from "../../../components/organisms/body-card"
 import InviteModal from "../../../components/organisms/invite-modal"
 import PlusIcon from "../../../components/fundamentals/icons/plus-icon"
-import EditIcon from "../../../components/fundamentals/icons/edit-icon"
-import SidebarTeamMember from "../../../components/molecules/sidebar-team-member"
-import useMedusa from "../../../hooks/use-medusa"
 import UserTable from "../../../components/templates/user-table"
 
-type UserListElement = {
-  entity: any
-  entityType: string
-  tableElement: JSX.Element
-}
-
-const getInviteStatus = invite => {
-  return new Date(invite.expires_at) < new Date() ? "expired" : "pending"
-}
-
 const Users: React.FC = () => {
-  const [elements, setElements] = useState<UserListElement[]>([])
-  const [shownElements, setShownElements] = useState<UserListElement[]>([])
   const [users, setUsers] = useState([])
   const [invites, setInvites] = useState([])
   const [shouldRefetch, setShouldRefetch] = useState(0)
-  const [selectedUser, setSelectedUser] = useState(null)
-  const [deleteUser, setDeleteUser] = useState(false)
-  const [selectedInvite, setSelectedInvite] = useState(null)
   const [showInviteModal, setShowInviteModal] = useState(false)
-  const { toaster } = useMedusa("store")
-
-  const handleClose = () => {
-    setDeleteUser(false)
-    setSelectedUser(null)
-    setSelectedInvite(null)
-  }
 
   const triggerRefetch = () => {
     setShouldRefetch(prev => prev + 1)
