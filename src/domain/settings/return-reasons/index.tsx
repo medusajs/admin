7760c6d3e5a400964f924d1c8492ec2fb2f6bbd3
zakariaelas <zakaria.elas@gmail.com>
2022-01-17 22:20:22 +0100

    fix: selection after deletion

--- src/domain/settings/return-reasons/index.tsx
+++ src/domain/settings/return-reasons/index.tsx
@@ -1,10 +1,10 @@
 import { useAdminReturnReasons } from "medusa-react"
-import React, { useEffect, useMemo, useState } from "react"
+import React, { useState } from "react"
+import Spinner from "../../../components/atoms/spinner"
 import PlusIcon from "../../../components/fundamentals/icons/plus-icon"
 import BreadCrumb from "../../../components/molecules/breadcrumb"
 import BodyCard from "../../../components/organisms/body-card"
 import RadioGroup from "../../../components/organisms/radio-group"
-import Spinner from "../../../components/atoms/spinner"
 import TwoSplitPane from "../../../components/templates/two-split-pane"
 import useModal from "../../../hooks/use-modal"
 import CreateReturnReasonModal from "./create-reason-modal"
@@ -12,21 +12,21 @@ import ReturnReasonDetail from "./detail"
 
 const ReturnReasons = () => {
   const { isOpen, handleOpen, handleClose } = useModal()
-  const { isLoading, isSuccess, return_reasons } = useAdminReturnReasons()
-  const sorted_return_reasons = useMemo(
-    () =>
-      return_reasons?.sort((a, b) => (a.created_at < b.created_at ? -1 : 1)),
-    [return_reasons]
-  )
+  const [selectedReason, setSelectedReason] = useState<any>(null)
+  const { isLoading, return_reasons } = useAdminReturnReasons({
+    onSuccess: (data) => {
+      // sorting is done in place
+      sortByCreatedAt(data.return_reasons)
 
-  const [selectedReason, setSelectedReason] = useState(null)
-
-  useEffect(() => {
-    // if success && is not already selected && sorted_reasons is not empty
-    if (isSuccess && !selectedReason && sorted_return_reasons.length > 0) {
-      setSelectedReason(sorted_return_reasons[0])
-    }
-  }, [sorted_return_reasons])
+      const newReturnReasons = data.return_reasons
+      // if this is the first time we fetch this list
+      // or if this is a refetch after a deletion
+      // then set the first element as the selected reason
+      if (!selectedReason || isADeletion(return_reasons, newReturnReasons)) {
+        setSelectedReason(newReturnReasons[0])
+      }
+    },
+  })
 
   return (
     <div>
@@ -58,14 +58,12 @@ const ReturnReasons = () => {
               </div>
             ) : (
               <RadioGroup
-                onValueChange={value =>
-                  setSelectedReason(
-                    findReasonByValue(sorted_return_reasons, value)
-                  )
+                onValueChange={(value) =>
+                  setSelectedReason(findReasonByValue(return_reasons, value))
                 }
                 value={selectedReason?.value}
               >
-                {sorted_return_reasons.map(reason => (
+                {return_reasons?.map((reason) => (
                   <RadioGroup.Item
                     label={reason.label}
                     sublabel={`(Value: ${reason.value})`}
@@ -78,15 +76,21 @@ const ReturnReasons = () => {
             )}
           </div>
         </BodyCard>
-        {selectedReason && <ReturnReasonDetail reason={selectedReason} /> }
+        {selectedReason && <ReturnReasonDetail reason={selectedReason} />}
       </TwoSplitPane>
       {isOpen && <CreateReturnReasonModal handleClose={handleClose} />}
     </div>
   )
 }
 
+const isADeletion = (returnReasons, newReturnReasons) =>
+  returnReasons && returnReasons?.length > newReturnReasons?.length
+
+const sortByCreatedAt = <T extends Record<string, any>>(returnReasons: T[]) =>
+  returnReasons?.sort((a, b) => (a.created_at < b.created_at ? -1 : 1))
+
 const findReasonByValue = (reasons, value) => {
-  return reasons.find(reason => reason.value === value)
+  return reasons.find((reason) => reason.value === value)
 }
 
 export default ReturnReasons
