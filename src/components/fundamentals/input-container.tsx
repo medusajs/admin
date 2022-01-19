7a4638ec80121710cc0a4b1fa9bcb7429b6a0d72
Zakaria El Asri <33696020+zakariaelas@users.noreply.github.com>
2022-01-19 15:32:57 +0100

    feat: revamp textarea (#248)

--- src/components/fundamentals/input-container.tsx
+++ src/components/fundamentals/input-container.tsx
@@ -23,14 +23,13 @@ const InputContainer: React.FC<InputContainerProps> = ({
       key={key}
       tabIndex={-1}
       onClick={onClick}
-      onBlur={e => {
+      onBlur={(e) => {
         if (onFocusLost && !e.currentTarget.contains(e.relatedTarget)) {
           onFocusLost()
         }
       }}
       className={clsx([
         `bg-grey-5 inter-base-regular w-full p-3 flex h-18 flex-col cursor-text border border-grey-20 focus-within:shadow-input focus-within:border-violet-60 rounded-base`,
-        ,
         className,
       ])}
     >
