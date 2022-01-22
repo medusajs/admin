6d9e4a6174ff3c28bc26e2b950c24a67d5b687e2
olivermrbl <oliver@mrbltech.com>
2022-01-22 22:01:58 +0100

    Adds collapsible

--- src/domain/gift-cards/manage/index.tsx
+++ src/domain/gift-cards/manage/index.tsx
@@ -10,6 +10,7 @@ import BreadCrumb from "../../../components/molecules/breadcrumb"
 import Input from "../../../components/molecules/input"
 import TagInput from "../../../components/molecules/tag-input"
 import BodyCard from "../../../components/organisms/body-card"
+import DetailsCollapsible from "../../../components/organisms/details-collapsible"
 import useToaster from "../../../hooks/use-toaster"
 import { getErrorMessage } from "../../../utils/error-messages"
 import DenominationTable from "./denomination-table"
@@ -57,7 +58,7 @@ const ManageGiftCard: React.FC<ManageGiftCardProps> = ({ id }) => {
           subtitle="Manage the settings for your Gift Card"
           className={"h-auto w-full"}
         >
-          <div className="flex flex-col space-y-8">
+          <div className="flex flex-col space-y-6">
             <div className="flex space-x-8">
               <div className="flex flex-col w-1/2 space-y-4">
                 <Input
@@ -82,31 +83,33 @@ const ManageGiftCard: React.FC<ManageGiftCardProps> = ({ id }) => {
                 onChange={(e) => setValue("description", e.target.value)}
               />
             </div>
-            <div className="flex space-x-8">
-              <div className="flex flex-col w-1/2 space-y-4">
-                <Input
-                  label="Handle"
-                  name="handle"
-                  defaultValue={giftCard?.title}
-                  onChange={(e) => setValue("title", e.target.value)}
-                />
-                <Input
-                  label="Type"
-                  name="type"
-                  placeholder="Add a subtitle"
-                  defaultValue={giftCard?.subtitle}
-                  onChange={(e) => setValue("subtitle", e.target.value)}
+            <DetailsCollapsible>
+              <div className="flex space-x-8">
+                <div className="flex flex-col w-1/2 space-y-4">
+                  <Input
+                    label="Handle"
+                    name="handle"
+                    defaultValue={giftCard?.title}
+                    onChange={(e) => setValue("title", e.target.value)}
+                  />
+                  <Input
+                    label="Type"
+                    name="type"
+                    placeholder="Add a subtitle"
+                    defaultValue={giftCard?.subtitle}
+                    onChange={(e) => setValue("subtitle", e.target.value)}
+                  />
+                </div>
+                <TagInput
+                  label="Tags (separated by comma)"
+                  placeholder={"sprint, summer"}
+                  defaultValue={giftCard?.tags.join(", ")}
+                  className="w-1/2"
+                  values={[]}
+                  onChange={(e) => setValue("description", e.target.value)}
                 />
               </div>
-              <TagInput
-                label="Tags (separated by comma)"
-                placeholder={"sprint, summer"}
-                defaultValue={giftCard?.tags.join(", ")}
-                className="w-1/2"
-                values={[]}
-                onChange={(e) => setValue("description", e.target.value)}
-              />
-            </div>
+            </DetailsCollapsible>
           </div>
         </BodyCard>
         <BodyCard
