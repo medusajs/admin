3ee3d7c53b4a65d9f4c89290edf9b293b4a848c8
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2022-01-24 15:47:20 +0100

    feat: revamp banner card (#243)

---
+++ src/hooks/use-observe-width.ts
@@ -0,0 +1,21 @@
+import { MutableRefObject, useEffect, useRef, useState } from "react"
+
+export const useObserveWidth = (ref: MutableRefObject<any>): number => {
+  const [currentWidth, setCurrentWidth] = useState(0)
+
+  const observer = useRef(
+    new ResizeObserver((entries) => {
+      const { width } = entries[0].contentRect
+
+      setCurrentWidth(width)
+    })
+  )
+
+  useEffect(() => {
+    observer.current.observe(ref.current)
+
+    return () => observer.current.unobserve(ref.current)
+  }, [ref, observer])
+
+  return currentWidth
+}
