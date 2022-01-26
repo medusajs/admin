a1751b820a35b02480731ef581e962e2b85f4dbf
Philip Korsholm <philip.korsholm@hotmail.com>
2022-01-26 08:51:28 +0100

    camel case svg

--- src/components/fundamentals/icons/chevron-left-icon/index.tsx
+++ src/components/fundamentals/icons/chevron-left-icon/index.tsx
@@ -18,9 +18,9 @@ const ChevronLeftIcon: React.FC<IconProps> = ({
       <path
         d="M9 12L5 8L9 4"
         stroke={color}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
