f6123d967955c936344934e56a23a56e891edcf8
zakariaelas <zakaria.elas@gmail.com>
2022-01-31 22:17:46 +0100

    fix: edit denominations modal

--- src/components/organisms/edit-denominations-modal/index.tsx
+++ src/components/organisms/edit-denominations-modal/index.tsx
@@ -1,61 +1,74 @@
 import React from "react"
-import { useFieldArray, useForm } from "react-hook-form"
 import { v4 as uuidv4 } from "uuid"
-import CurrencyInput from "../../organisms/currency-input"
 import Button from "../../fundamentals/button"
 import PlusIcon from "../../fundamentals/icons/plus-icon"
 import TrashIcon from "../../fundamentals/icons/trash-icon"
 import InfoTooltip from "../../molecules/info-tooltip"
 import Modal from "../../molecules/modal"
+import CurrencyInput from "../../organisms/currency-input"
 
-type Denomination = {
-  currencyCode: string
+export type PriceType = {
+  currency_code: string
   amount: number
-  id: string
+  id?: string
 }
 
 type EditDenominationsModalProps = {
-  denominations: Denomination[]
+  defaultDenominations: PriceType[]
   handleClose: () => void
-  onSubmit: (denominations: Denomination[]) => void
+  onSubmit: (denominations: PriceType[]) => void
   defaultNewAmount?: number
   defaultNewCurrencyCode?: string
   currencyCodes?: string[]
 }
 
-type FormValues = {
-  denominations: Denomination[]
+const augmentWithId = (obj) => ({ ...obj, id: obj.id ? obj.id : uuidv4() })
+
+const augmentWithIds = (list) => {
+  return list.map(augmentWithId)
 }
 
 const EditDenominationsModal = ({
-  denominations,
+  defaultDenominations = [],
   onSubmit,
   handleClose,
   currencyCodes,
   defaultNewAmount = 1000,
   defaultNewCurrencyCode = "USD",
 }: EditDenominationsModalProps) => {
-  const { control } = useForm<FormValues>({
-    defaultValues: {
-      denominations,
-    },
-  })
-  const { fields, append, remove, update } = useFieldArray({
-    control,
-    name: "denominations",
-    keyName: "native_key",
-  })
+  const [denominations, setDenominations] = React.useState(
+    augmentWithIds(defaultDenominations)
+  )
 
   const onAmountChange = (index) => {
     return (amount) => {
-      update(index, { ...fields[index], amount })
+      const newDenominations = denominations.slice()
+      newDenominations[index] = { ...newDenominations[index], amount }
+      setDenominations(newDenominations)
     }
   }
 
   const onCurrencyChange = (index) => {
     return (currencyCode) => {
-      update(index, { ...fields[index], currencyCode })
+      const newDenominations = denominations.slice()
+      newDenominations[index] = {
+        ...newDenominations[index],
+        currency_code: currencyCode,
+      }
+      setDenominations(newDenominations)
     }
+  }
+
+  const onClickDelete = (index) => {
+    return () => {
+      const newDenominations = denominations.slice()
+      newDenominations.splice(index, 1)
+      setDenominations(newDenominations)
+    }
+  }
+
+  const appendDenomination = (newDenomination) => {
+    setDenominations([...denominations, augmentWithId(newDenomination)])
   }
 
   return (
@@ -72,7 +85,7 @@ const EditDenominationsModal = ({
               </label>
               <InfoTooltip content={"Helpful denominations"} />
             </div>
-            {fields.map((field, index) => {
+            {denominations.map((field, index) => {
               return (
                 <div
                   key={field.id}
@@ -81,8 +94,8 @@ const EditDenominationsModal = ({
                   <div className="flex-1">
                     <CurrencyInput
                       currencyCodes={currencyCodes}
+                      currentCurrency={field.currency_code}
                       onChange={onCurrencyChange(index)}
-                      currentCurrency={field.currencyCode}
                     >
                       <CurrencyInput.AmountInput
                         label="Amount"
@@ -93,7 +106,7 @@ const EditDenominationsModal = ({
                   </div>
                   <button className="ml-2xlarge">
                     <TrashIcon
-                      onClick={() => remove(index)}
+                      onClick={onClickDelete(index)}
                       className="text-grey-40"
                       size="20"
                     />
@@ -105,10 +118,9 @@ const EditDenominationsModal = ({
           <div className="mt-large">
             <Button
               onClick={() =>
-                append({
+                appendDenomination({
+                  currency_code: defaultNewCurrencyCode,
                   amount: defaultNewAmount,
-                  currencyCode: defaultNewCurrencyCode,
-                  id: uuidv4(),
                 })
               }
               type="button"
@@ -134,7 +146,7 @@ const EditDenominationsModal = ({
               variant="primary"
               size="small"
               className="mr-2 min-w-[130px] justify-center"
-              onClick={() => onSubmit(fields)}
+              onClick={() => onSubmit(denominations)}
             >
               Save
             </Button>
