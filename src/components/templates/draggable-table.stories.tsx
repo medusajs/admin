8352bfc3a8d8fba0822f3eeae60450085d4a2f4f
Philip Korsholm <philip.korsholm@hotmail.com>
2022-02-02 17:25:06 +0100

    cleanup story

--- src/components/templates/draggable-table.stories.tsx
+++ src/components/templates/draggable-table.stories.tsx
@@ -2,59 +2,6 @@ import { ComponentMeta, ComponentStory, storiesOf } from "@storybook/react"
 import React, { useState } from "react"
 import DraggableTable from "./draggable-table"
 
-// export default {
-//   title: "Organisms/DraggableTable",
-//   component: DraggableTable,
-// } as ComponentMeta<typeof DraggableTable>
-
-// const Template: ComponentStory<typeof DraggableTable> = (args) => (
-//   <div className="h-large w-large">
-//     <DraggableTable {...args} />
-//   </div>
-// )
-
-// const [entities, setEntities] = useState([
-//   {
-//     id: "1",
-//     role: "member",
-//   },
-//   {
-//     id: "2",
-//     role: "member",
-//   },
-//   {
-//     id: "3",
-//     role: "member",
-//   },
-//   {
-//     id: "4",
-//     role: "member",
-//   },
-//   {
-//     id: "5",
-//     role: "member",
-//   },
-//   {
-//     id: "6",
-//     role: "member",
-//   },
-//   {
-//     id: "7",
-//     role: "member",
-//   },
-// ])
-
-const columns = [
-  {
-    Header: "ID",
-    accessor: "id",
-  },
-  {
-    Header: "Role",
-    accessor: "role",
-  },
-]
-
 storiesOf("Templates/DraggableTable", module).add("DraggableTable", () => {
   const [entities, setEntities] = useState([
     {
@@ -86,6 +33,18 @@ storiesOf("Templates/DraggableTable", module).add("DraggableTable", () => {
       role: "member",
     },
   ])
+
+  const columns = [
+    {
+      Header: "ID",
+      accessor: "id",
+    },
+    {
+      Header: "Role",
+      accessor: "role",
+    },
+  ]
+
   return (
     <DraggableTable
       entities={entities}
@@ -102,14 +61,4 @@ const Template: ComponentStory<typeof DraggableTable> = (args) => (
 )
 
 export const Default = Template.bind({})
-DraggableTable.args = {
-  // entities: entities,
-  // setEntities: setEntities,
-  columns: columns,
-}
-
-// <DraggableTable
-//   entities={entities}
-//   setEntities={setEntities}
-//   columns={columns}
-// />
+DraggableTable.args = {}
