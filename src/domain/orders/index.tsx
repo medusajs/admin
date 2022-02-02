b6475a9328abbe3d6ddbd9d4189369ebf0616909
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2022-02-02 21:43:30 +0100

    feat: Draft Order list overview (#291)

--- src/domain/orders/index.tsx
+++ src/domain/orders/index.tsx
@@ -1,18 +1,52 @@
 import { RouteComponentProps, Router } from "@reach/router"
-import React from "react"
+import clsx from "clsx"
+import { capitalize } from "lodash"
+import React, { useState } from "react"
 import BodyCard from "../../components/organisms/body-card"
+import DraftOrderTable from "../../components/templates/draft-order-table"
 import OrderTable from "../../components/templates/order-table"
 import Details from "./details"
 import DraftOrderDetails from "./draft-orders/details"
 import NewOrder from "./new"
 
+const VIEWS = [
+  "overview",
+  "drafts",
+  // "swaps", <- TODO
+  // "returns" <- TODO
+]
+
 const OrderIndex: React.FC<RouteComponentProps> = () => {
+  const [view, setView] = useState("overview")
+
+  const CurrentView = () => {
+    switch (view) {
+      case "drafts":
+        return <DraftOrderTable />
+      default:
+        return <OrderTable />
+    }
+  }
+
   return (
     <div className="flex flex-col grow h-full">
       <div className="w-full flex flex-col grow">
-        <BodyCard title="Overview" className="mb-0">
+        <BodyCard className="mb-0">
+          <div className="flex inter-large-semibold text-grey-40 space-x-4">
+            {VIEWS.map((k, i) => (
+              <div
+                key={i}
+                className={clsx("cursor-pointer", {
+                  ["text-grey-90"]: k === view,
+                })}
+                onClick={() => setView(k)}
+              >
+                {capitalize(k)}
+              </div>
+            ))}
+          </div>
           <div className="flex grow  flex-col pt-2 mt-large">
-            <OrderTable />
+            <CurrentView />
           </div>
         </BodyCard>
       </div>
