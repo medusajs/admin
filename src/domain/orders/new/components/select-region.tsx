4b556502f83e068715ed593dc0c646f010473e30
Philip Korsholm <philip.korsholm@hotmail.com>
2022-02-05 16:32:00 +0100

    update components of new draft order modal

---
+++ src/domain/orders/new/components/select-region.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react"
+import Select from "../../../../components/molecules/select"
+
+const SelectRegionScreen = ({ handleRegionSelect, region, options }) => {
+  const { enableNextPage, disableNextPage } = React.useContext(SteppedContext)
+
+  useEffect(() => {
+    if (!region) {
+      disableNextPage()
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col min-h-[705px]">
+      <span className="inter-base-semibold mb-4">Choose region</span>
+      <Select
+        label={"Region"}
+        value={region ? { value: region, label: region.name } : null}
+        onChange={(option) => {
+          if (option) {
+            enableNextPage()
+          } else {
+            disableNextPage()
+          }
+          handleRegionSelect(option)
+        }}
+        options={options}
+      />
+    </div>
+  )
+}
+
+export default SelectRegionScreen
