13337eb8d3f63c739a6dc93d29ca35a56aa25f33
Kasper <kasper@medusajs.com>
2022-02-08 10:19:08 +0100

    add hook to highlight search terms

---
+++ src/hooks/use-highlight-search.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx"
+import React from "react"
+
+export const useHighlightSearch = (query: string) => {
+  function getHighlightedSearch(text: string) {
+    const parts = text.split(new RegExp(`(${query})`, "gi"))
+    return (
+      <span>
+        {" "}
+        {parts.map((part: string, i: number) => (
+          <span
+            key={i}
+            className={clsx({
+              "bg-orange-10": part.toLowerCase() === query.toLowerCase(),
+            })}
+          >
+            {part}
+          </span>
+        ))}{" "}
+      </span>
+    )
+  }
+
+  return { getHighlightedSearch }
+}
