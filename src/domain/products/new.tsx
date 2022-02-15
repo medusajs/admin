0adf99a040e7affb6531517c0a09628582c40810
Zakaria El Asri <33696020+zakariaelas@users.noreply.github.com>
2022-02-15 15:59:45 +0100

    fix: missing product details (#360)
    * add: publish/unpublish + delete product
    
    * add: allow discounts on product new/details

--- src/domain/products/new.tsx
+++ src/domain/products/new.tsx
@@ -47,7 +47,7 @@ const NewProductPage = () => {
   return (
     <ProductFormProvider onSubmit={onSubmit}>
       <ProductForm />
-      <div className="mt-base flex justify-end items-center gap-x-2">
+      <div className="mt-base pb-xlarge flex justify-end items-center gap-x-2">
         <Button
           variant="secondary"
           size="small"
