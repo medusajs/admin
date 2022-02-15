9c233c0bb099dea1ac4faf584b4c6f9578e10198
Philip Korsholm <philip.korsholm@hotmail.com>
2022-02-15 13:10:25 +0100

    initial revamp before design

--- src/pages/invite.js
+++ src/pages/invite.js
@@ -7,10 +7,8 @@ import jwt from "jsonwebtoken"
 import LoginLayout from "../components/login-layout"
 import SEO from "../components/seo"
 import InputField from "../components/molecules/input"
-import Button from "../components/button"
-import { ReactComponent as Graphic } from "../assets/login-graphic.svg"
+import Button from "../components/fundamentals/button"
 import Medusa from "../services/api"
-import useMedusa from "../hooks/use-medusa"
 import MedusaIcon from "../components/fundamentals/icons/medusa-icon"
 
 const InvitePage = ({ location }) => {
@@ -52,103 +50,101 @@ const InvitePage = ({ location }) => {
   return (
     <LoginLayout pt={3}>
       <SEO title="Login" />
-      <Flex
-        width="100%"
-        height="100%"
-        flexDirection="column"
-        justifyContent="center"
-        sx={{ position: "relative" }}
-      >
-        <Flex
-          backgroundColor="#fefefe"
-          width={5 / 12}
-          sx={{ zIndex: 9999 }}
-          flexDirection="column"
-          alignItems="center"
-          mx="auto"
-          as="form"
-          height="400px"
-          onSubmit={acceptInvite}
-        >
-          <Box width={1} p={4} justifyContent="center">
-            <div className="w-full flex justify-center mb-8">
-              <MedusaIcon />
-            </div>
-            {!token ? (
-              <Flex flexDirection="column" alignItems="center">
-                <Text mb={2} fontWeight="bold" fontSize={4}>
-                  You signup link is invalid
-                </Text>
-                <Text mb={2} fontSize={2}>
-                  Contact your administrator to obtain a valid signup link
-                </Text>
-              </Flex>
-            ) : (
-              <>
-                <Text mb={4} fontWeight="bold" fontSize={4}>
-                  You have been invited to join the Team!
-                </Text>
-                <Flex width={1} flexDirection="column" alignItems="center">
-                  <InputField
-                    mb={3}
-                    width="100%"
-                    label="Email"
-                    name="email"
-                    value={email}
-                    inputStyle={{
-                      color: "#454545",
-                      backgroundColor: "#f0f0f0",
-                    }}
-                    disabled={true}
-                    onChange={(e) => setEmail(e.target.value)}
-                    boldLabel={true}
-                  />
-                  <Flex width={1} justifyContent="space-between">
+      <div className="flex w-full h-full justify-center items-center">
+        <div className="flex bg-grey-0 min-h-[600px] rounded-rounded">
+          <form onSubmit={acceptInvite}>
+            <div className="p-12 flex flex-col items-center">
+              <div className="w-full flex justify-center mb-8">
+                <MedusaIcon />
+              </div>
+              {!token ? (
+                <div className="flex flex-col items-center">
+                  <span className="inter-xlarge-semibold mb-4 text-grey-90">
+                    You signup link is invalid
+                  </span>
+                  <span className="inter-large-regular mb-4 text-grey-90">
+                    Contact your administrator to obtain a valid signup link
+                  </span>
+                </div>
+              ) : (
+                <>
+                  <span className="inter-xlarge-semibold mb-4 text-grey-90">
+                    You have been invited to join the Team!
+                  </span>
+                  <div className="grid w-full gap-y-small">
                     <InputField
                       mb={3}
-                      mr={3}
-                      width="50%"
-                      label="First name"
+                      width="100%"
+                      label="Email"
+                      name="email"
+                      required
+                      value={email}
+                      inputStyle={{
+                        color: "#454545",
+                        backgroundColor: "#f0f0f0",
+                      }}
+                      disabled={true}
+                      onChange={(e) => setEmail(e.target.value)}
                       boldLabel={true}
-                      value={firstName}
-                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                    <div className="grid grid-cols-2 gap-x-small">
+                      <InputField
+                        mb={3}
+                        mr={3}
+                        width="50%"
+                        placeholder="First name..."
+                        label="First name"
+                        boldLabel={true}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                      />
+                      <InputField
+                        mb={3}
+                        width="50%"
+                        placeholder="Last name..."
+                        label="Last name"
+                        boldLabel={true}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                      />
+                    </div>
+                    <InputField
+                      mb={3}
+                      type="password"
+                      width="100%"
+                      required
+                      placeholder="Password..."
+                      label="Password"
+                      boldLabel={true}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                     />
                     <InputField
                       mb={3}
-                      width="50%"
-                      label="Last name"
+                      type="password"
+                      width="100%"
+                      required
+                      placeholder="Repeat password..."
+                      label="Repeat password"
                       boldLabel={true}
-                      value={lastName}
-                      onChange={(e) => setLastName(e.target.value)}
+                      value={repeatPassword}
+                      onChange={(e) => setRepeatPassword(e.target.value)}
                     />
-                  </Flex>
-                  <InputField
-                    mb={3}
-                    type="password"
-                    width="100%"
-                    label="Password"
-                    boldLabel={true}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <InputField
-                    mb={3}
-                    type="password"
-                    width="100%"
-                    label="Repeat password"
-                    boldLabel={true}
-                    value={repeatPassword}
-                    onChange={(e) => setRepeatPassword(e.target.value)}
-                  />
-                  <Button type="submit" variant={"cta"} mt={4} width={1 / 4}>
+                  </div>
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    size="large"
+                    className="rounded-rounded mt-8 w-full inter-base-regular"
+                  >
                     Login
                   </Button>
-                </Flex>
-              </>
-            )}
-          </Box>
-        </Flex>
-      </Flex>
+                </>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
     </LoginLayout>
   )
 }
