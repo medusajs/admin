f4113d8af7209269ee2da0d40a64918025cce93b
Sebastian Rindom <skrindom@gmail.com>
2022-02-22 14:19:11 +0100

    feat: tax settings (#383)
    * fix: adds tax-rate table
    
    * fix: adds tax rates settings
    
    * fix: use canary tax-api packages
    
    * fix: use canary tax-api packages
    
    * fix: use imperative dialog
    
    * fix: add delete rule
    
    * fix: use indeterminate checkbox molecule

---
+++ src/domain/settings/taxes/index.tsx
@@ -0,0 +1,111 @@
+import { useAdminRegions } from "medusa-react"
+import { navigate } from "gatsby"
+import React, { useEffect, useState } from "react"
+import Spinner from "../../../components/atoms/spinner"
+import GearIcon from "../../../components/fundamentals/icons/gear-icon"
+import BreadCrumb from "../../../components/molecules/breadcrumb"
+import BodyCard from "../../../components/organisms/body-card"
+import RadioGroup from "../../../components/organisms/radio-group"
+import TwoSplitPane from "../../../components/templates/two-split-pane"
+import TaxDetails from "./details"
+
+const Taxes = () => {
+  const { regions, isLoading, refetch } = useAdminRegions()
+  const [selectedRegion, setSelectedRegion] = useState<string | undefined>(
+    undefined
+  )
+
+  useEffect(() => {
+    if (!isLoading && regions && selectedRegion === null) {
+      setSelectedRegion(regions[0].id)
+    }
+  }, [regions, isLoading, selectedRegion])
+
+  const handleDelete = () => {
+    refetch().then(({ data }) => {
+      const id = data?.regions?.[0]?.id
+
+      if (!id) {
+        return
+      }
+
+      setSelectedRegion(id)
+      document.getElementById(id)?.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      })
+    })
+  }
+
+  const handleSelect = (id: string) => {
+    refetch().then(() => {
+      setSelectedRegion(id)
+      document.getElementById(id)?.scrollIntoView({
+        behavior: "smooth",
+      })
+    })
+  }
+
+  return (
+    <>
+      <div>
+        <BreadCrumb
+          previousRoute="/a/settings"
+          previousBreadcrumb="Settings"
+          currentPage="Taxes"
+        />
+        <TwoSplitPane threeCols>
+          <BodyCard
+            forceDropdown
+            title="Regions"
+            subtitle="Select the region you wish to manage taxes for"
+            actionables={[
+              {
+                icon: <GearIcon />,
+                label: "Go to Region settings",
+                onClick: () => navigate("/a/settings/regions"),
+              },
+            ]}
+          >
+            {isLoading || !regions ? (
+              <div className="flex-grow h-full flex items-center justify-center">
+                <Spinner size="large" variant="secondary" />
+              </div>
+            ) : (
+              <RadioGroup.Root
+                value={selectedRegion}
+                onValueChange={setSelectedRegion}
+              >
+                {regions.map((r) => {
+                  return (
+                    <RadioGroup.Item
+                      label={r.name}
+                      description={
+                        r.countries.length
+                          ? `${r.countries
+                              .map((c) => c.display_name)
+                              .join(", ")}`
+                          : undefined
+                      }
+                      value={r.id}
+                      key={r.id}
+                      id={r.id}
+                    />
+                  )
+                })}
+              </RadioGroup.Root>
+            )}
+          </BodyCard>
+          <TaxDetails
+            id={selectedRegion}
+            onDelete={handleDelete}
+            handleSelect={handleSelect}
+          />
+        </TwoSplitPane>
+      </div>
+    </>
+  )
+}
+
+export default Taxes
---
+++ src/domain/settings/taxes/region-form.tsx
@@ -0,0 +1,156 @@
+import React, { useEffect, useMemo } from "react"
+import { Controller, useForm } from "react-hook-form"
+import { useAdminUpdateRegion, useAdminStoreTaxProviders } from "medusa-react"
+
+import useNotification from "../../../hooks/use-notification"
+import Button from "../../../components/fundamentals/button"
+import Checkbox from "../../../components/atoms/checkbox"
+import InfoTooltip from "../../../components/molecules/info-tooltip"
+import { getErrorMessage } from "../../../utils/error-messages"
+import Select from "../../../components/molecules/select"
+
+export const RegionTaxForm = ({ region }) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { isDirty },
+  } = useForm({
+    defaultValues: {
+      automatic_taxes: region.automatic_taxes,
+      gift_cards_taxable: region.gift_cards_taxable,
+      tax_provider_id: {
+        label:
+          region.tax_provider_id === null
+            ? "System Tax Provider"
+            : region.tax_provider_id,
+        value: region.tax_provider_id,
+      },
+    },
+  })
+  const notification = useNotification()
+
+  useEffect(() => {
+    reset({
+      automatic_taxes: region.automatic_taxes,
+      gift_cards_taxable: region.gift_cards_taxable,
+      tax_provider_id: {
+        label:
+          region.tax_provider_id === null
+            ? "System Tax Provider"
+            : region.tax_provider_id,
+        value: region.tax_provider_id,
+      },
+    })
+  }, [region])
+
+  const {
+    isLoading: isProvidersLoading,
+    tax_providers,
+  } = useAdminStoreTaxProviders()
+
+  const updateRegion = useAdminUpdateRegion(region.id)
+
+  const providerOptions = useMemo(() => {
+    if (tax_providers) {
+      return [
+        {
+          label: "System Tax Provider",
+          value: null,
+        },
+        ...tax_providers.map((tp) => ({
+          label: tp.id,
+          value: tp.id,
+        })),
+      ]
+    }
+    return [
+      {
+        label: "System Tax Provider",
+        value: null,
+      },
+    ]
+  }, [tax_providers])
+
+  const onSubmit = (data) => {
+    const toSubmit = {
+      ...data,
+      tax_provider_id: data.tax_provider_id.value,
+    }
+
+    updateRegion.mutate(toSubmit, {
+      onSuccess: () => {
+        notification(
+          "Success",
+          "Region tax settings were successfully updated.",
+          "success"
+        )
+      },
+      onError: (error) => {
+        notification("Error", getErrorMessage(error), "error")
+      },
+    })
+  }
+
+  return (
+    <form className="flex flex-col flex-1" onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col gap-base flex-1">
+        <Controller
+          name="tax_provider_id"
+          control={control}
+          defaultValue={region.tax_provider_id}
+          rules={{ required: true }}
+          render={(props) => (
+            <Select
+              disabled={isProvidersLoading}
+              label="Tax Provider"
+              options={providerOptions}
+              value={props.value}
+              onChange={props.onChange}
+              className="mb-base"
+            />
+          )}
+        />
+        <div className="flex item-center gap-x-1.5">
+          <Checkbox
+            className="inter-base-regular"
+            name="automatic_taxes"
+            ref={register}
+            label="Calculate taxes automatically?"
+          />
+          <InfoTooltip
+            content={
+              "When checked Medusa will automatically apply tax calculations to Carts in this Region. When unchecked you will have to manually compute taxes at checkout. Manual taxes are recommended if using a 3rd party tax provider to avoid performing too many requests"
+            }
+          />
+        </div>
+        <div className="flex item-center gap-x-1.5">
+          <Checkbox
+            className="inter-base-regular"
+            name="gift_cards_taxable"
+            ref={register}
+            label="Apply tax to gift cards?"
+          />
+          <InfoTooltip
+            content={
+              "When checked taxes will be applied to gift cards on checkout. In some contries tax regulations require that taxes are applied to gift cards on purchase."
+            }
+          />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        {isDirty && (
+          <Button
+            loading={updateRegion.isLoading}
+            variant="primary"
+            size="medium"
+            type="submit"
+          >
+            Save
+          </Button>
+        )}
+      </div>
+    </form>
+  )
+}
