b882f3fb03471d2f1421d2fb623a9b3f34b865a2
fPolic <frane@medusajs.com>
2022-03-03 22:48:31 +0100

    feat: setup customer group section on customers page

---
+++ src/domain/customers/groups/index.ts
@@ -0,0 +1,1 @@
+export { default as CustomerGroupTable } from "./list"
--- src/domain/customers/index.tsx
+++ src/domain/customers/index.tsx
@@ -1,16 +1,29 @@
-import { RouteComponentProps, Router } from "@reach/router"
 import React from "react"
+import { RouteComponentProps, Router } from "@reach/router"
+
 import BodyCard from "../../components/organisms/body-card"
 import TableViewHeader from "../../components/organisms/custom-table-header"
 import CustomerTable from "../../components/templates/customer-table"
+import { CustomerGroupTable } from "./groups"
 import Details from "./details"
 
 const CustomerIndex: React.FC<RouteComponentProps> = () => {
+  const [activeView, setActiveView] = React.useState("customers")
+
+  const isGroupsView = activeView === "customers"
   return (
     <div className="flex flex-col grow h-full">
       <div className="w-full flex flex-col grow">
-        <BodyCard customHeader={<TableViewHeader views={["customers"]} />}>
-          <CustomerTable />
+        <BodyCard
+          customHeader={
+            <TableViewHeader
+              activeView={activeView}
+              setActiveView={setActiveView}
+              views={["customers", "groups"]}
+            />
+          }
+        >
+          {isGroupsView ? <CustomerTable /> : <CustomerGroupTable />}
         </BodyCard>
       </div>
     </div>
