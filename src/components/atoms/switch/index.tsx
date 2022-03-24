dbae44095868eb7482567f6d58b3aca8e05b8dd1
fPolic <frane@medusajs.com>
2022-03-24 11:36:46 +0100

    feat: Switch atom & story

--- src/components/atoms/switch/index.tsx
+++ src/components/atoms/switch/index.tsx
@@ -1,14 +1,29 @@
 import React from "react"
-import * as Switch from "@radix-ui/react-switch"
+import clsx from "clsx"
+import * as _Switch from "@radix-ui/react-switch"
 
-type P = {
-  checked: boolean
-}
+/**
+ * A controlled switch component atom.
+ */
+function Switch(props: _Switch.SwitchProps) {
+  const { checked } = props
 
-export default function (props: P) {
   return (
-    <Switch.Root>
-      <Switch.Thumb />
-    </Switch.Root>
+    <_Switch.Root
+      {...props}
+      className={clsx("w-8 h-[18px] rounded-full", {
+        "bg-gray-300": !checked,
+        "bg-violet-60": checked,
+      })}
+    >
+      <_Switch.Thumb
+        className={clsx("w-2 h-2 bg-white rounded-full block transition-all", {
+          "translate-x-[5px] ": !checked,
+          "translate-x-[19px] bg-violet-60": checked,
+        })}
+      />
+    </_Switch.Root>
   )
 }
+
+export default Switch
--- src/components/atoms/switch/switch.stories.tsx
+++ src/components/atoms/switch/switch.stories.tsx
@@ -1,14 +1,28 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-import Switch from "."
+import Switch from "./"
 
 export default {
   title: "Atoms/Switch",
   component: Switch,
 } as ComponentMeta<typeof Switch>
 
-const Template: ComponentStory<typeof Switch> = (args) => <Switch {...args} />
+const Template: ComponentStory<typeof Switch> = (args) => {
+  const [checked, setChecked] = useState(args.checked)
+
+  useEffect(() => {
+    setChecked(args.checked)
+  }, [args.checked])
+
+  return (
+    <Switch
+      {...args}
+      checked={checked}
+      onCheckedChange={(c) => setChecked(c)}
+    />
+  )
+}
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = { checked: true }
