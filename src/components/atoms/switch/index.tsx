8fdb58e4de2e70deef7fe1c1d2e417ba6f3fb697
fPolic <frane@medusajs.com>
2022-03-24 13:10:31 +0100

    fix: address PR feedback

--- src/components/atoms/switch/index.tsx
+++ src/components/atoms/switch/index.tsx
@@ -1,28 +1,31 @@
 import React from "react"
 import clsx from "clsx"
-import * as _Switch from "@radix-ui/react-switch"
+import * as RadixSwitch from "@radix-ui/react-switch"
 
 /**
  * A controlled switch component atom.
  */
-function Switch(props: _Switch.SwitchProps) {
+function Switch(props: RadixSwitch.SwitchProps) {
   const { checked } = props
 
   return (
-    <_Switch.Root
+    <RadixSwitch.Root
       {...props}
-      className={clsx("w-8 h-[18px] rounded-full", {
+      className={clsx("w-8 h-[18px] rounded-full transition-bg", {
         "bg-gray-300": !checked,
         "bg-violet-60": checked,
       })}
     >
-      <_Switch.Thumb
-        className={clsx("w-2 h-2 bg-white rounded-full block transition-all", {
-          "translate-x-[5px] ": !checked,
-          "translate-x-[19px] bg-violet-60": checked,
-        })}
+      <RadixSwitch.Thumb
+        className={clsx(
+          "w-2 h-2 bg-white rounded-full block transition-transform",
+          {
+            "translate-x-[5px] ": !checked,
+            "translate-x-[19px] bg-violet-60": checked,
+          }
+        )}
       />
-    </_Switch.Root>
+    </RadixSwitch.Root>
   )
 }
 
