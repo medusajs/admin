03f136f4aff4335fbd98c92a53730ee469553978
Zakaria El Asri <zakaria.elas@gmail.com>
2022-03-29 09:49:02 +0000

    fix: remove redundant HotKeyAction component

--- src/components/molecules/table-toaster/index.tsx
+++ src/components/molecules/table-toaster/index.tsx
@@ -1,7 +1,6 @@
 import clsx from "clsx"
 import React from "react"
 import { Toast } from "react-hot-toast"
-import { useHotkeys } from "react-hotkeys-hook"
 
 export type TableToasterContainerProps = {
   children: React.ReactElement[] | React.ReactElement | React.ReactNode
@@ -22,18 +21,6 @@ export const TableToasterContainer = ({
     >
       <div className="flex items-center rounded-rounded bg-grey-90 px-base py-3.5">
         {children}
-      </div>
-    </div>
-  )
-}
-
-export const HotKeyAction = ({ label, hotKey, icon, onAction }) => {
-  useHotkeys(hotKey, onAction, {})
-  return (
-    <div className="flex items-center gap-2">
-      <span className="text-grey-0 inter-small-semibold">{label}</span>
-      <div className="inter-small-semibold text-grey-30 flex items-center justify-center w-[24px] h-[24px] rounded bg-grey-70">
-        {icon}
       </div>
     </div>
   )
