371c04957777ebfe31fb6f10f2bb71a585576daa
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-03-30 16:09:58 +0200

    Update src/components/templates/discount-table/use-promotion-columns.tsx
    Co-authored-by: Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>

--- src/components/templates/discount-table/use-promotion-columns.tsx
+++ src/components/templates/discount-table/use-promotion-columns.tsx
@@ -198,7 +198,7 @@ const getNumberOfConditions = (condition) => {
       return condition.product_collections.length
     case "product_tags":
       return condition.product_tags.length
-    case "customer_group":
+    case "customer_groups":
       return condition.customer_groups.length
     default:
       return null
