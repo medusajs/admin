e7c859a83214b154410a7a97e18ef61f1253a288
fPolic <frane@medusajs.com>
2022-04-06 13:37:00 +0200

    feat: init file

---
+++ src/domain/discounts/discount-form/condition-tables/tags.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+/**
+ * A table for selecting product tags for a product condition.
+ */
+function SelectTagsTable() {
+  return <div></div>
+}
+
+export default SelectTagsTable
