7f9a07c032993d479ee7173f2481c09123145ec6
fPolic <frane@medusajs.com>
2022-04-13 11:11:16 +0200

    feat: pass info about selected state to label component

--- src/components/molecules/table-fileds-filter/table-field-filters.stories.tsx
+++ src/components/molecules/table-fileds-filter/table-field-filters.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentMeta } from "@storybook/react"
 import React from "react"
+import clsx from "clsx"
+
 import TableFieldsFilters from "./index"
 
 export default {
@@ -15,27 +17,42 @@ Default.args = {
     {
       id: "p-usd",
       short: "Price USD",
-      label: (
+      label: ({ isSelected }) => (
         <span className="text-small text-grey-50">
-          <span className="font-semibold text-grey-90">USD</span> (USA)
+          <span
+            className={clsx("text-grey-90", { "font-semibold": isSelected })}
+          >
+            USD{" "}
+          </span>
+          (USA)
         </span>
       ),
     },
     {
       id: "p-dkk",
       short: "Price DKK",
-      label: (
+      label: ({ isSelected }) => (
         <span className="text-small text-grey-50">
-          <span className="font-semibold text-grey-90">DKK</span> (Denmark)
+          <span
+            className={clsx("text-grey-90", { "font-semibold": isSelected })}
+          >
+            DKK{" "}
+          </span>
+          (Denmark)
         </span>
       ),
     },
     {
       id: "p-hrk",
       short: "Price HRK",
-      label: (
+      label: ({ isSelected }) => (
         <span className="text-small text-grey-50">
-          <span className="font-semibold text-grey-90">HRK</span> (Coratia)
+          <span
+            className={clsx("text-grey-90", { "font-semibold": isSelected })}
+          >
+            HRK{" "}
+          </span>
+          (Croatia)
         </span>
       ),
     },
