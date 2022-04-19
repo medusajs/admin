0b0a48fffbcd07463abc58231ce51759d697735c
fPolic <frane@medusajs.com>
2022-04-19 13:20:35 +0200

    wip: setup modal

---
+++ src/domain/pricing/bulk-editor/index.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+import Fade from "../../../components/atoms/fade-wrapper"
+import FocusModal from "../../../components/molecules/modal/focus-modal"
+import Button from "../../../components/fundamentals/button"
+import CrossIcon from "../../../components/fundamentals/icons/cross-icon"
+
+function BulkEditorContainer() {
+  return (
+    <Fade isVisible isFullScreen={true}>
+      <FocusModal>
+        <FocusModal.Header>
+          <div className="medium:w-8/12 w-full px-8 flex justify-between">
+            <div className="flex items-center gap-4">
+              <Button
+                size="small"
+                variant="ghost"
+                // onClick={closeForm}
+                className="border rounded-rounded w-8 h-8"
+              >
+                <CrossIcon size={20} />
+              </Button>
+              <span className="font-semibold text-grey-90 text-large">
+                Bulk editor
+              </span>
+            </div>
+            <div className="gap-x-small flex">
+              <Button
+                // onClick={handleSubmit(submitGhost)}
+                size="small"
+                variant="ghost"
+                className="border rounded-rounded"
+              >
+                Discard
+              </Button>
+              <Button
+                size="small"
+                // variant="primary"
+                // onClick={handleSubmit(submitCTA)}
+                className="rounded-rounded"
+              >
+                Save
+              </Button>
+            </div>
+          </div>
+        </FocusModal.Header>
+        <FocusModal.Main>
+          {/*<div className="absolute w-full bottom-0">asd</div>*/}
+        </FocusModal.Main>
+      </FocusModal>
+    </Fade>
+  )
+}
+
+export default BulkEditorContainer
--- src/domain/pricing/index.tsx
+++ src/domain/pricing/index.tsx
@@ -1,18 +1,21 @@
 import { RouteComponentProps, Router } from "@reach/router"
 import { navigate } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import PlusIcon from "../../components/fundamentals/icons/plus-icon"
 import BodyCard from "../../components/organisms/body-card"
 import TableViewHeader from "../../components/organisms/custom-table-header"
 import PricingDetails from "./details"
 import New from "./new"
 import PricingTable from "./pricing-table"
+import BulkEditorContainer from "./bulk-editor"
 
 const PricingIndex: React.FC<RouteComponentProps> = () => {
+  const [show, setShow] = useState(false) // TODO: remove
+
   const actionables = [
     {
       label: "Add price list",
-      onClick: () => navigate(`/a/pricing/new`),
+      onClick: () => setShow(true),
       icon: <PlusIcon size={20} />,
     },
   ]
@@ -27,6 +30,7 @@ const PricingIndex: React.FC<RouteComponentProps> = () => {
           <PricingTable />
         </BodyCard>
       </div>
+      {show && <BulkEditorContainer />}
     </div>
   )
 }
