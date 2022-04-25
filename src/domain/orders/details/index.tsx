c01798d0b280d78646ecdc7b4ef88daf8d206870
Kasper <kasper@medusajs.com>
2022-04-25 13:32:02 +0200

    add copy to gc code

--- src/domain/orders/details/index.tsx
+++ src/domain/orders/details/index.tsx
@@ -13,6 +13,7 @@ import React, { useMemo, useState } from "react"
 import { useHotkeys } from "react-hotkeys-hook"
 import ReactJson from "react-json-view"
 import Avatar from "../../../components/atoms/avatar"
+import CopyToClipboard from "../../../components/atoms/copy-to-clipboard"
 import Spinner from "../../../components/atoms/spinner"
 import Tooltip from "../../../components/atoms/tooltip"
 import Badge from "../../../components/fundamentals/badge"
@@ -135,12 +136,12 @@ const OrderDetails = ({ id }) => {
 
   const notification = useNotification()
 
-  const [, handleCopy] = useClipboard(order?.display_id, {
+  const [, handleCopy] = useClipboard(`${order?.display_id!}`, {
     successDuration: 5500,
     onCopied: () => notification("Success", "Order ID copied", "success"),
   })
 
-  const [, handleCopyEmail] = useClipboard(order?.email, {
+  const [, handleCopyEmail] = useClipboard(order?.email!, {
     successDuration: 5500,
     onCopied: () => notification("Success", "Email copied", "success"),
   })
@@ -254,7 +255,7 @@ const OrderDetails = ({ id }) => {
     {
       label: "Go to Customer",
       icon: <DetailsIcon size={"20"} />,
-      onClick: () => navigate(`/a/customers/${order.customer.id}`),
+      onClick: () => navigate(`/a/customers/${order?.customer.id}`),
     },
   ]
 
@@ -390,6 +391,13 @@ const OrderDetails = ({ id }) => {
                         <Badge className="ml-3" variant="default">
                           {giftCard.code}
                         </Badge>
+                        <div className="ml-2">
+                          <CopyToClipboard
+                            value={giftCard.code}
+                            showValue={false}
+                            iconSize={16}
+                          />
+                        </div>
                       </div>
                     }
                   />
