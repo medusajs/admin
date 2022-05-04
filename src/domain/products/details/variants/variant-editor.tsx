2a84f35ba55d1ff1029c8e6994c2732cc03aada2
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2022-05-04 09:50:16 +0200

    fix: bugs related to creating variant (#548)

--- src/domain/products/details/variants/variant-editor.tsx
+++ src/domain/products/details/variants/variant-editor.tsx
@@ -12,6 +12,7 @@ import Select from "../../../../components/molecules/select"
 import CurrencyInput from "../../../../components/organisms/currency-input"
 import { convertEmptyStringToNull } from "../../../../utils/convert-empty-string-to-null"
 import { countries as countryData } from "../../../../utils/countries"
+import { focusByName } from "../../../../utils/focus-by-name"
 import usePricesFieldArray from "../../product-form/form/usePricesFieldArray"
 
 const defaultVariant = {
@@ -79,6 +80,15 @@ const VariantEditor = ({
   }, [variant, store])
 
   const handleSave = (data) => {
+    if (!data.prices) {
+      focusByName("add-price")
+      return
+    }
+
+    if (!data.title) {
+      data.title = data.options.map((o) => o.value).join(" / ")
+    }
+
     data.prices = data.prices.map(({ price: { currency_code, amount } }) => ({
       currency_code,
       amount: Math.round(amount),
@@ -131,6 +141,7 @@ const VariantEditor = ({
                   <Input
                     ref={register({ required: true })}
                     name={`options[${index}].value`}
+                    required={true}
                     label={field.title}
                     defaultValue={field.value}
                   />
@@ -147,9 +158,10 @@ const VariantEditor = ({
           <div className="mb-8">
             <label
               tabIndex={0}
-              className="inter-base-semibold mb-4 flex items-center gap-xsmall"
+              className="inter-base-semibold mb-4 flex items-center"
             >
               {"Prices"}
+              <span className="text-rose-50 mr-xsmall">*</span>
               <IconTooltip content={"Variant prices"} />
             </label>
 
@@ -208,6 +220,7 @@ const VariantEditor = ({
               onClick={appendPrice}
               size="small"
               variant="ghost"
+              name="add-price"
               disabled={availableCurrencies?.length === 0}
             >
               <PlusIcon size={20} /> Add a price
