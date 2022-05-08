fc9dff9c516b82e8a4d43b69e0572f2d0ee058c2
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-05-08 17:48:30 +0700

    fix: update border radius on input (#553)

--- src/components/molecules/table/table-search.tsx
+++ src/components/molecules/table/table-search.tsx
@@ -30,7 +30,7 @@ const TableSearch: React.FC<TableSearchProps> = ({
   return (
     <div
       className={clsx(
-        "inter-small-regular mt-1 transition-color transition-width duration-150 ease-in-out flex text-grey-50 flex items-center mb-1 pl-1 py-1.5 rounded border border-grey-0 min-w-content focus-within:mr-1 focus-within:w-60 focus-within:shadow-input focus-within:border-violet-60 focus-within:bg-grey-5",
+        "inter-small-regular mt-1 transition-color transition-width duration-150 ease-in-out flex text-grey-50 flex items-center mb-1 pl-1 py-1.5 rounded-rounded border border-grey-0 min-w-content focus-within:mr-1 focus-within:w-60 focus-within:shadow-input focus-within:border-violet-60 focus-within:bg-grey-5",
         className
       )}
       {...props}
