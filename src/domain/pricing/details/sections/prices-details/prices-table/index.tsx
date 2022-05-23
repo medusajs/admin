2359f87f4829a6632251b12697976ae3f0fb915c
Zakaria El Asri <zakaria.elas@gmail.com>
2022-05-23 13:15:05 +0100

    fix: use bulk editor in edit prices

--- src/domain/pricing/details/sections/prices-details/prices-table/index.tsx
+++ src/domain/pricing/details/sections/prices-details/prices-table/index.tsx
@@ -1,13 +1,15 @@
 import { Product } from "@medusajs/medusa"
-import { useAdminPriceListProducts } from "medusa-react"
+import { useAdminPriceList, useAdminPriceListProducts } from "medusa-react"
 import * as React from "react"
 import { HeaderGroup, Row } from "react-table"
-import CancelIcon from "../../../../../../components/fundamentals/icons/cancel-icon"
+// import CancelIcon from "../../../../../../components/fundamentals/icons/cancel-icon"
 import EditIcon from "../../../../../../components/fundamentals/icons/edit-icon"
 import Table from "../../../../../../components/molecules/table"
 import { SelectableTable } from "../../../../../../components/templates/selectable-table"
 import useQueryFilters from "../../../../../../hooks/use-query-filters"
+import useToggleState from "../../../../../../hooks/use-toggle-state"
 import usePricesColumns from "./use-columns"
+import BulkEditor from "../../../../bulk-editor"
 
 const DEFAULT_PAGE_SIZE = 9
 const defaultQueryProps = {
@@ -26,7 +28,9 @@ const PricesTable = ({ id, selectProduct }: PricesTableProps) => {
     id,
     params.queryObject
   )
+  const { price_list } = useAdminPriceList(id)
   const columns = usePricesColumns()
+  const [showBulkEditor, openBulkEditor, closeBulkEditor] = useToggleState()
 
   return (
     <div className="w-full overflow-y-auto flex flex-col justify-between min-h-[300px] h-full ">
@@ -42,20 +46,21 @@ const PricesTable = ({ id, selectProduct }: PricesTableProps) => {
             {
               label: "Edit prices",
               icon: <EditIcon size={20} />,
-              onClick: handleSelect,
+              onClick: openBulkEditor,
             },
-            {
-              label: "Remove product",
-              icon: <CancelIcon size={20} />,
-              variant: "danger" as const,
-              onClick: () => {},
-            },
+            // {
+            //   label: "Remove product",
+            //   icon: <CancelIcon size={20} />,
+            //   variant: "danger" as const,
+            //   onClick: () => {},
+            // },
           ]
 
           return (
             <Table.Row
               {...row.getRowProps()}
               actions={actions}
+              forceDropdown
               onClick={handleSelect}
               className="hover:bg-grey-5 hover:cursor-pointer"
             >
@@ -78,6 +83,9 @@ const PricesTable = ({ id, selectProduct }: PricesTableProps) => {
         }}
         {...params}
       />
+      {showBulkEditor && price_list && (
+        <BulkEditor priceList={price_list} closeEditor={closeBulkEditor} />
+      )}
     </div>
   )
 }
