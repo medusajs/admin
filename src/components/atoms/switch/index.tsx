76c39de6bfbbacb2ae1052d1f7167aa8d17d8c3a
Kasper <kasper@medusajs.com>
2022-05-31 10:21:42 +0200

    fix switch

--- src/components/atoms/switch/index.tsx
+++ src/components/atoms/switch/index.tsx
@@ -2,26 +2,22 @@ import * as RadixSwitch from "@radix-ui/react-switch"
 import clsx from "clsx"
 import React from "react"
 
-const Switch = (props: RadixSwitch.SwitchProps) => {
-  const { checked } = props
-
+/**
+ * A controlled switch component atom.
+ */
+function Switch(props: RadixSwitch.SwitchProps) {
   return (
     <RadixSwitch.Root
       {...props}
-      asChild={true}
-      className={clsx("w-8 h-[18px] rounded-full transition-bg", {
-        "bg-gray-300": !checked,
-        "bg-violet-60": checked,
-      })}
+      className={clsx(
+        "w-8 h-[18px] rounded-full transition-bg bg-gray-300 radix-state-checked:bg-violet-60"
+      )}
+      asChild
     >
       <div className="flex items-center">
         <RadixSwitch.Thumb
           className={clsx(
-            "w-2 h-2 bg-white rounded-full block transition-transform",
-            {
-              "translate-x-[5px] ": !checked,
-              "translate-x-[19px] bg-violet-60": checked,
-            }
+            "w-2 h-2 bg-white rounded-full block transition-transform translate-x-[5px] radix-state-checked:translate-x-[19px]"
           )}
         />
       </div>
