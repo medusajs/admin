a93670d2b081f34e2e1d3312c82ecb67536698cf
adrien2p <adrien.deperetti@gmail.com>
2022-06-24 10:12:56 +0200

    feat: Move polling to context provider API

---
+++ src/components/molecules/download-file/index.tsx
@@ -0,0 +1,22 @@
+import FileIcon from "../../fundamentals/icons/file-icon"
+import Button, { ButtonProps } from "../../fundamentals/button"
+import React from "react"
+
+type Props = ButtonProps & {
+  fileName: string
+  fileSize: string
+}
+
+const DownloadFileButton = ({ fileName, fileSize, ...buttonProps }: Props) => {
+  return (
+    <Button className="flex justify-start mt-4" {...buttonProps}>
+      <FileIcon size={30}/>
+      <div className="flex flex-col text-left">
+        <span className="overflow-hidden truncate w-4/5">{fileName}</span>
+        <span className="text-grey-40">{fileSize}</span>
+      </div>
+    </Button>
+  )
+}
+
+export default DownloadFileButton
