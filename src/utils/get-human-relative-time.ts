970935cd3034cd0fa3d56352614f8e0631f05539
adrien2p <adrien.deperetti@gmail.com>
2022-06-28 14:34:34 +0200

    feat: Update get relative time utils

--- src/utils/get-human-relative-time.ts
+++ src/utils/get-human-relative-time.ts
@@ -10,12 +10,15 @@ const getRelativeTime = (dates: { from: Date | string; to: Date | string }) => {
   const elapsed = new Date(dates.to).getTime() - new Date(dates.from).getTime()
 
   for (const [unit, displayedUnit, amount] of units) {
-    if (Math.abs(elapsed) > amount || unit === "minute") {
-      const isLessThan1Min = Math.abs(elapsed) <= amount
+    if (Math.abs(elapsed) >= amount || unit === "minute") {
+      const isLessThan1Min = Math.abs(elapsed) <= amount && unit === "minute"
       const suffix = elapsed <= 0 ? "ago" : ""
       const prefix = elapsed > 0 ? "in" : ""
       const indicator = Math.abs(elapsed) < 1 ? "<" : ""
-      const timeToShow = Math.max(1, Math.abs(Math.round(elapsed / amount)))
+      const timeToShow = Math.max(
+        1,
+        Math.abs(Math.round(elapsed / amount))
+      )
 
       return {
         raw: elapsed,
