e69e6e3c0b2ded7ce1b99b525f0dc9cb72c2ce5d
Frane Polić <16856471+fPolic@users.noreply.github.com>
2022-08-02 12:25:25 +0200

    Update src/domain/sales-channels/tables/placeholder.tsx
    Co-authored-by: Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>

--- src/domain/sales-channels/tables/placeholder.tsx
+++ src/domain/sales-channels/tables/placeholder.tsx
@@ -6,7 +6,9 @@ import SidedMouthFaceIcon from "../../../components/fundamentals/icons/sided-mou
 function Placeholder({ showAddModal }) {
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      <SidedMouthFaceIcon color="grey-50" width="48" height="48" />
+      <span className="text-grey-50">
+        <SidedMouthFaceIcon width="48" height="48" />
+      </span>
 
       <h3 className="font-semibold text-large text-gray-90 mt-6">
         Start building your channels setup...
