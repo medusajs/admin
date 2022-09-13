1aa970b0c34df8f1e5d3dd89cbaba02202d42c21
Philip Korsholm <88927411+pKorsholm@users.noreply.github.com>
2022-09-13 10:30:46 +0200

    Feat(admin): Toggle tax inclusivity for price lists (#685)

--- src/domain/pricing/pricing-form/sections/general.tsx
+++ src/domain/pricing/pricing-form/sections/general.tsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { Controller } from "react-hook-form"
+import Switch from "../../../../components/atoms/switch"
+import FeatureToggle from "../../../../components/fundamentals/feature-toggle"
 import InputField from "../../../../components/molecules/input"
 import Accordion from "../../../../components/organisms/accordion"
 import { usePriceListForm } from "../form/pricing-form-context"
 
 const General = () => {
-  const { register } = usePriceListForm()
+  const { control, register } = usePriceListForm()
 
   return (
     <Accordion.Item
@@ -19,14 +22,31 @@ const General = () => {
           label="Name"
           required
           placeholder="B2B, Black Friday..."
-          {...register("name",{ required: "Name is required" })}
+          {...register("name", { required: "Name is required" })}
         />
         <InputField
           label="Description"
           required
           placeholder="For our business partners..."
-          {...register("description",{ required: "Description is required" })}
+          {...register("description", { required: "Description is required" })}
         />
+        <FeatureToggle featureFlag="tax_inclusive_pricing">
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <h2 className="inter-base-semibold">Tax inclusive prices</h2>
+              <Controller
+                control={control}
+                name="includes_tax"
+                render={({ field: { value, onChange } }) => {
+                  return <Switch checked={!!value} onCheckedChange={onChange} />
+                }}
+              />
+            </div>
+            <p className="inter-base-regular text-grey-50">
+              Choose to make all prices in this list inclusive of tax.
+            </p>
+          </div>
+        </FeatureToggle>
       </div>
     </Accordion.Item>
   )
