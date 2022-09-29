5bfd9c0bf085fbdf04791ce9c389dbd9770f7ccd
Kasper <kasper@medusajs.com>
2022-09-29 17:45:55 +0200

    split into two components

--- src/components/molecules/amount-input/amount-input.stories.tsx
+++ src/components/molecules/amount-input/amount-input.stories.tsx
@@ -1,28 +1,34 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react"
 import React from "react"
-import AmountInput from "."
+import AmountAndCurrencyInput from "./amount-and-currency-input"
+import AmountInput from "./amount-input"
 
 export default {
-  title: "Molecules/AmountInput",
-  component: AmountInput,
+  title: "Molecules/AmountInput/AmountInput",
+  component: AmountAndCurrencyInput,
 } as ComponentMeta<typeof AmountInput>
 
 const Template: ComponentStory<typeof AmountInput> = (props) => {
+  const [value, setValue] = React.useState<number | undefined | null>(1000)
+
   return (
     <div className="w-[280px]">
-      <AmountInput {...props} />
+      <AmountInput
+        {...props}
+        onChange={setValue}
+        value={value}
+        currencyCode="usd"
+      />
+      <div className="mt-base rounded-rounded bg-grey-5 py-xsmall px-small">
+        <pre className="mono-small-regular">
+          {JSON.stringify(value, null, 2)}
+        </pre>
+      </div>
     </div>
   )
 }
 
-export const Default = Template.bind({})
-Default.args = {
+export const Amount = Template.bind({})
+Amount.args = {
   label: "Price",
-  value: {
-    amount: 100,
-    currency: {
-      label: "USD",
-      value: "USD",
-    },
-  },
 }
