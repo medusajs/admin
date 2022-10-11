e92a8bf07ba714a0b70b072a327e4f40b25233d4
Philip Korsholm <philip.korsholm@hotmail.com>
2022-10-11 18:02:05 +0800

    remove unnecessary nested private routes

--- src/pages/a.js
+++ src/pages/a.js
@@ -32,17 +32,17 @@ const Routes = () => {
       <Layout>
         <SEO title="Medusa" />
         <Router basepath="a" className="h-full">
-          <PrivateRoute path="oauth/:app_name" component={Oauth} />
-          <PrivateRoute path="products/*" component={ProductsRoute} />
-          <PrivateRoute path="collections/*" component={Collections} />
-          <PrivateRoute path="gift-cards/*" component={GiftCards} />
-          <PrivateRoute path="orders/*" component={Orders} />
-          <PrivateRoute path="draft-orders/*" component={DraftOrders} />
-          <PrivateRoute path="discounts/*" component={Discounts} />
-          <PrivateRoute path="customers/*" component={Customers} />
-          <PrivateRoute path="pricing/*" component={Pricing} />
-          <PrivateRoute path="settings/*" component={Settings} />
-          <PrivateRoute path="sales-channels/*" component={SalesChannels} />
+          <Oauth path="oauth/:app_name"/>
+          <ProductsRoute path="products/*"/>
+          <Collections path="collections/*"/>
+          <GiftCards path="gift-cards/*" />
+          <Orders path="orders/*" />
+          <DraftOrders path="draft-orders/*" />
+          <Discounts path="discounts/*"/>
+          <Customers path="customers/*"/>
+          <Pricing path="pricing/*"/>
+          <Settings path="settings/*"/>
+          <SalesChannels path="sales-channels/*" />
         </Router>
       </Layout>
     </DndProvider>
