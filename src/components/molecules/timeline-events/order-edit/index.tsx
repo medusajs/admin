8b31f5fabe397013f2cba2a977598583f0c06c85
fPolic <frane@medusajs.com>
2022-10-18 11:44:57 +0200

    fix: displaying user info

--- src/components/molecules/timeline-events/order-edit/index.tsx
+++ src/components/molecules/timeline-events/order-edit/index.tsx
@@ -2,14 +2,26 @@ import React from "react"
 import Avatar from "../../../atoms/avatar"
 
 type ByLineProps = {
-  user: { first_name: string; last_name: string; email: string }
+  user?: { first_name: string; last_name: string; email: string }
 }
-export const ByLine: React.FC<ByLineProps> = ({ user }) => (
-  <div className="flex inter-small-regular items-center text-grey-50">
-    By
-    <span className="w-base h-base mx-xsmall">
-      <Avatar user={user} font="inter-xsmall-semibold" />
-    </span>
-    {`${user?.first_name || ""} ${user?.last_name || ""}`}
-  </div>
-)
+
+export const ByLine: React.FC<ByLineProps> = ({ user }) => {
+  if (!user) {
+    return null
+  }
+
+  const { first_name, last_name, email } = user
+
+  const by =
+    !first_name && !last_name ? email : `${first_name || ""} ${last_name || ""}`
+
+  return (
+    <div className="flex inter-small-regular items-center text-grey-50">
+      By
+      <span className="w-base h-base mx-xsmall">
+        <Avatar user={user} font="inter-xsmall-semibold" />
+      </span>
+      {by}
+    </div>
+  )
+}
