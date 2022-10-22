d4150d4758463c2099737151e4f155edaf0e605f
Crow <5488190+echocrow@users.noreply.github.com>
2022-10-23 00:07:47 +0200

    chore: remove obsolete storybook gatsby shim

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,4 +1,3 @@
-import { action } from "@storybook/addon-actions"
 import { Toaster } from "react-hot-toast"
 import "../src/assets/styles/global.css"
 
@@ -10,19 +9,6 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
-
-global.___loader = {
-  enqueue: () => {},
-  hovering: () => {},
-}
-// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
-global.__PATH_PREFIX__ = ""
-global.__BASE_PATH__ = "/"
-
-// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
-window.___navigate = (pathname) => {
-  action("NavigateTo:")(pathname)
 }
 
 export const decorators = [
