5ba67598003355ee5690e721d4c7649d6b185810
Crow <5488190+echocrow@users.noreply.github.com>
2022-10-23 00:09:18 +0200

    chore: avoid var name ambiguity

--- src/components/atoms/save-notification/index.tsx
+++ src/components/atoms/save-notification/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react"
 import type { Toast } from "react-hot-toast"
-import { toast as global } from "react-hot-toast"
+import { toast as globalToast } from "react-hot-toast"
 import RefreshIcon from "../../fundamentals/icons/refresh-icon"
 import ToasterContainer from "../toaster-container"
 import ErrorState from "./error-state"
@@ -26,24 +26,30 @@ const SaveNotification: React.FC<SaveNotificationProps> = ({
 }) => {
   const onDismiss = () => {
     reset()
-    global.dismiss(toast.id)
+    globalToast.dismiss(toast.id)
   }
 
   const handleSave = () => {
-    global.custom((t) => <SavingState toast={t} />, {
+    globalToast.custom((t) => <SavingState toast={t} />, {
       id: toast.id,
     })
 
     onSave()
       .then(() => {
-        global.custom((t) => <SuccessState toast={t} onDismiss={onDismiss} />, {
-          id: toast.id,
-        })
+        globalToast.custom(
+          (t) => <SuccessState toast={t} onDismiss={onDismiss} />,
+          {
+            id: toast.id,
+          }
+        )
       })
       .catch((_err) => {
-        global.custom((t) => <ErrorState toast={t} onDismiss={onDismiss} />, {
-          id: toast.id,
-        })
+        globalToast.custom(
+          (t) => <ErrorState toast={t} onDismiss={onDismiss} />,
+          {
+            id: toast.id,
+          }
+        )
       })
   }
 
