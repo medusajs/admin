1a8016726856e823f9cce47fe3126769cd48bdbe
Crow <5488190+echocrow@users.noreply.github.com>
2022-10-22 15:25:19 +0200

    fix: pass params to route in reach router shim

--- src/compat/reach-router-compat.tsx
+++ src/compat/reach-router-compat.tsx
@@ -1,22 +1,33 @@
-import type { ReactNode } from "react"
-import { Routes, Route } from "react-router-dom"
+import { cloneElement, ReactElement } from "react"
+import { Routes, Route, useParams } from "react-router-dom"
 
 export { useLocation, useParams } from "react-router-dom"
 export type { PathRouteProps as RouteComponentProps } from "react-router-dom"
 
-type RouterProps = {
-  children: ReactNode
+/**
+ * @deprecated
+ */
+const RouteElement = ({ el }: { el: ReactElement }) => {
+  const params = useParams()
+  return cloneElement(el, params)
 }
 
+type RouterProps = {
+  children: ReactElement
+}
 /**
  * @deprecated
  */
 export const Router = ({ children }: RouterProps) => {
-  const routes = Array.isArray(children) ? children : [children]
+  const routes: ReactElement[] = Array.isArray(children) ? children : [children]
   return (
     <Routes>
       {routes.map((route) => (
-        <Route path={route.props.path} element={route} key={route.props.path} />
+        <Route
+          path={route.props.path}
+          element={<RouteElement el={route} />}
+          key={route.props.path}
+        />
       ))}
     </Routes>
   )
