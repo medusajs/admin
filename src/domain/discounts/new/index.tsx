390aa474d9d067133069414863fbab5932d2fef1
Crow <5488190+echocrow@users.noreply.github.com>
2022-11-28 23:11:42 +0100

    refactor: remove dead discount clone code

--- src/domain/discounts/new/index.tsx
+++ src/domain/discounts/new/index.tsx
@@ -1,16 +1,11 @@
-import { Discount } from "@medusajs/medusa"
-import { useLocation } from "react-router-dom"
 import DiscountForm from "./discount-form"
 import { DiscountFormProvider } from "./discount-form/form/discount-form-context"
 
 const New = () => {
-  const location = useLocation()
-  const toDuplicate = location?.state?.discount as Discount | undefined
-
   return (
     <div className="pb-xlarge">
       <DiscountFormProvider>
-        <DiscountForm discount={toDuplicate} />
+        <DiscountForm />
       </DiscountFormProvider>
     </div>
   )
