2fd1b17c6b3a3596c7fd2406ba7e5defb1a3b94e
adrien2p <adrien.deperetti@gmail.com>
2022-12-05 10:08:00 +0100

    cleanup

--- src/components/organisms/delete-prompt.tsx
+++ src/components/organisms/delete-prompt.tsx
@@ -31,12 +31,8 @@ const DeletePrompt: React.FC<DeletePromptProps> = ({
 
     setIsLoading(true)
     onDelete()
-      .then(() => {
-        notification("Success", successText, "success")
-      })
-      .catch((err) => {
-        notification("Error", getErrorMessage(err), "error")
-      })
+      .then(() => notification("Success", successText, "success"))
+      .catch((err) => notification("Error", getErrorMessage(err), "error"))
       .finally(() => {
         setIsLoading(false)
         handleClose()
