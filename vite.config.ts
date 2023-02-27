a9ebcaa641d680e4622c565092b4dbaf2c551559
romeosanjose <romeosanjose@gmail.com>
2023-02-27 01:04:08 +0000

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
-/// <reference types="vitest" />
-import react from "@vitejs/plugin-react"
-import dns from "dns"
 import path from "path"
 import { env } from "process"
-import { defineConfig } from "vite"
+import dns from "dns"
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
+import dotenv from 'dotenv'
 
 // Resolve localhost for Node v16 and older.
 // @see https://vitejs.dev/config/server-options.html#server-host.
 dns.setDefaultResultOrder("verbatim")
 
+dotenv.config()
+
+console.log("medusaa api",env.MEDUSA_BACKEND_URL)
+
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./src/test/setup.ts"],
-    api: 7001,
-  },
   // Backwards-compat with Gatsby.
   publicDir: "static",
   build: {
@@ -32,13 +30,15 @@ export default defineConfig({
     },
   },
   define: {
-    __MEDUSA_BACKEND_URL__: JSON.stringify(
-      env.MEDUSA_BACKEND_URL ||
-        // Backwards-compat with Gatsby.
-        env.GATSBY_MEDUSA_BACKEND_URL ||
-        env.GATSBY_STORE_URL ||
-        ""
-    ),
+//      __MEDUSA_BACKEND_URL__ : JSON.stringify({"MEDUSA_BACKEND_URL":"cs.local.api"}),	  
+   __MEDUSA_BACKEND_URL__: JSON.stringify(
+     env.MEDUSA_BACKEND_URL ||
+       // Backwards-compat with Gatsby.
+       env.GATSBY_MEDUSA_BACKEND_URL ||
+       env.GATSBY_STORE_URL ||
+       ""
+   ),
+       
   },
   optimizeDeps: {
     exclude: ["typeorm", "medusa-interfaces"],
