352ef11c5563a169c9f5aa34d08b11cdb32a81a2
Philip Korsholm <philip.korsholm@hotmail.com>
2023-03-01 19:14:14 +0100

    fixes

--- src/pages/a.jsx
+++ src/pages/a.jsx
@@ -20,8 +20,6 @@ import ProductsRoute from "../domain/products"
 import PublishableApiKeys from "../domain/publishable-api-keys"
 import SalesChannels from "../domain/sales-channels"
 import Settings from "../domain/settings"
-import PublishableApiKeys from "../domain/publishable-api-keys"
-import Inventory from "../domain/inventory"
 
 const IndexPage = () => {
   const navigate = useNavigate()
