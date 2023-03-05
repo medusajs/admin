a693cbe42415c20a4d61eb87b5b74083a77a2b53
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2023-03-05 18:08:32 +0100

    hotfix: add skus to claim tables (#911)
    * add skus to claim tables
    
    * add skus to return form
    
    * fix copy button

--- src/components/atoms/copy-to-clipboard/index.tsx
+++ src/components/atoms/copy-to-clipboard/index.tsx
@@ -32,13 +32,14 @@ const CopyToClipboard: React.FC<CopyToClipboardProps> = ({
     if (isCopied) {
       notification("Success", "Copied!", "success")
     }
-  }, [isCopied])
+  }, [isCopied, notification])
 
   return (
-    <div className="flex items-center inter-small-regular text-grey-50 gap-x-xsmall">
+    <div className="inter-small-regular flex items-center gap-x-xsmall text-grey-50">
       <Button
         variant="ghost"
         size="small"
+        type="button"
         className={clsx("p-0 text-grey-50", {
           ["text-violet-60"]: isCopied,
         })}
